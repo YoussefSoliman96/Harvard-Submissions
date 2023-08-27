c96f6a76b5bdededc5cc4e3ccebeb56855bb931b
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-08-27 08:29:56 +0000

    Sun, Aug 27, 2023, 8:29 AM +00:00

--- hello/hello.c
+++ hello/hello.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
+#include <cs50.h>
 
 int main(void)
 {
-    string n = get_string("Name: ");
-    printf("hello, world\n");
+    string n = get_string("Name: \n");
+    printf("hello, %s\n", n);
 }
