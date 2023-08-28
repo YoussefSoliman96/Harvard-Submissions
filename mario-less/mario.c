f55377853edb9781ca81d2663e08b4f46af96705
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-08-28 09:44:43 +0000

    Mon, Aug 28, 2023, 11:44 AM +02:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -3,24 +3,24 @@
 
 int main(void)
 {
-int h;
+    int h;
     do
     {
-    h = get_int("Height: ");
+        h = get_int("Height: ");
     }
     while (h < 1 || h > 8);
 
     for (int i = 0; i < h; i++)
     {
-        for (int k = 1; k < h - i;k++ )
+        for (int k = 1submit50 cs50/problems/2023/x/mario/less; k < h - i; k++)
         {
             printf(" ");
         }
-        for (int j = 0; j <= i; j++ )
+        for (int j = 0; j <= i; j++)
         {
             printf("#");
         }
 
-    printf("\n");
+        printf("\n");
     }
 }
