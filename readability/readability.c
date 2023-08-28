785aff81923cf07259501945e989e0b57a1a3749
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-08-28 05:59:26 +0000

    Mon, Aug 28, 2023, 7:59 AM +02:00

--- readability/readability.c
+++ readability/readability.c
@@ -3,8 +3,22 @@
 
 int main(void)
 {
+int letter_count (string text)
+
 //take user input
 string sentence = get_string("The sentence is: ");
 
 
+
+
+
+
+
+
+
+
+
+
+int letter_count (string text)
+if ()
 }
