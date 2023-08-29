e5e98ea7b695db2a65456fae6e4dbde5cd42f099
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-08-29 06:31:19 +0000

    Tue, Aug 29, 2023, 8:31 AM +02:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -1,5 +1,6 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
 
 // Max voters and candidates
 #define MAX_VOTERS 100
@@ -132,7 +133,7 @@ bool vote(int voter, int rank, string name)
     {
         if (strcmp(name, candidates[i].name) == 0)
         {
-            
+
         }
     }
     return false;
