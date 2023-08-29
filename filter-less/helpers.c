0305e4a7a2f87462559fe079e6e3aed46e7918a2
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-08-29 13:16:31 +0000

    Tue, Aug 29, 2023, 3:16 PM +02:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -3,14 +3,15 @@
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
-    for (i = 0; i < width; i++)
+    for (i = 0; i < height; i++)
     {
-        for(j = 0; j < height; j++)
+        for(j = 0; j < width; j++)
         {
-            if(image[j][i] == )
-            {
-                
-            }
+            float red = image[i][j].rgbtRed;
+            float blue = image[i][j].rgbtBlue;
+            float red = image[i][j].rgbtGreen;
+
+            int average = round (red + blue + green / 3);
         }
     }
     return;
