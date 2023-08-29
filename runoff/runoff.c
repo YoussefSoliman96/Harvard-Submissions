7f24929ab18980f7b1681bf375c946385c2cd856
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-08-29 06:37:48 +0000

    Tue, Aug 29, 2023, 8:37 AM +02:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -144,6 +144,13 @@ bool vote(int voter, int rank, string name)
 void tabulate(void)
 {
     // TODO
+    for(int i = 0; i < candidate_count; i++)
+    {
+        if (strcmp(name, candidates[i].name) == 0)
+        {
+            candidates[i].votes++;
+        }
+    }
     return;
 }
 
