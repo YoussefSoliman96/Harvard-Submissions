717a69f36232a849755fe1e9f718409350e01d4c
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-08-29 06:13:11 +0000

    Tue, Aug 29, 2023, 8:13 AM +02:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -128,6 +128,13 @@ int main(int argc, string argv[])
 bool vote(int voter, int rank, string name)
 {
     // TODO
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (strcmp(name, candidates[i].name) == 0)
+        {
+            
+        }
+    }
     return false;
 }
 
