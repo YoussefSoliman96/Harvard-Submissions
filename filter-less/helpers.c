5ad45dade70c9ff96cfd5d3eb374405ce449c750
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-08-29 13:39:40 +0000

    Tue, Aug 29, 2023, 3:39 PM +02:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -57,6 +57,13 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < height; j++)
+        {
+
+        }
+    }
     return;
 }
 
