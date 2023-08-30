26a0932fa28067f074c25030430ae4393c08708c
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-08-30 17:02:11 +0000

    Wed, Aug 30, 2023, 7:02 PM +02:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -64,22 +64,27 @@ person *create_family(int generations)
         p-> parents[0] = NULL;
         p-> parents[1] = NULL;
         // TODO: Randomly assign alleles
-        p-> alleles[0] = 
+        p-> alleles[0] = random_allele(;)
+        p-> alleles[1] = random_allele(;)
     }
 
     // TODO: Return newly created person
-    return NULL;
+    return p;
 }
 
 // Free `p` and all ancestors of `p`.
 void free_family(person *p)
 {
     // TODO: Handle base case
-
+    if (p == NULL)
+    {
+        return;
+    }
     // TODO: Free parents recursively
-
+    free_family(p-> parents[0]);
+    free_family(p-> parents[1]);
     // TODO: Free child
-
+    free(p);
 }
 
 // Print each family member and their alleles.
