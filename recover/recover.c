b408d8ffe5853411c1317c7dce3c93242bc6f7af
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-08-30 08:55:43 +0000

    Wed, Aug 30, 2023, 10:55 AM +02:00

--- recover/recover.c
+++ recover/recover.c
@@ -28,25 +28,20 @@ int main(int argc, char *argv[])
     FILE *output_file = NULL;
     // File name
     char *file_name = malloc (8 * sizeof(char));
-    if (buffer[0] == 0xff)
+    // Read data from the memory card
+    while (fread (buffer, sizeof(char), 512, input_file ))
     {
-        if (buffer[1] == 0xd8)
+        if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && buffer[3] && 0xf0) == 0xe0)
         {
-            if (buffer[2] == 0xff)
-            {
-            if ((buffer[3] && 0xf0) == 0xe0)
-            {
-                printf("file is JPEG");
-                // Make a new JPEG file
-                sprintf(filename, "%03i.jpg", 2)
-                // Open a new file with the new file name and write the data to it
-                FILE *img = fopen(filename, "w");
-                fwrite (data, size, number, outputr)
-            }
-            }
+                    printf("file is JPEG");
+                    // Make a new JPEG file
+                    sprintf(filename, "%03i.jpg", 2)
+                    // Open a new file with the new file name and write the data to it
+                    FILE *img = fopen(filename, "w");
+                    fwrite (data, size, number, outputr)
         }
     }
-    // Read data from the memory card
+
     fread (data, size, number, inptr);
 
 }
