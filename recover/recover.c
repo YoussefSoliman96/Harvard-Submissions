98309f9556a25526a65ab2eb27cd65e8e45ad720
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-08-30 08:39:37 +0000

    Wed, Aug 30, 2023, 10:39 AM +02:00

--- recover/recover.c
+++ recover/recover.c
@@ -3,32 +3,33 @@
 
 int main(int argc, char *argv[])
 {
-// Open memory card
-FILE *f = fopen (card.raw, "r");
+    
+    // Open memory card
+    FILE *f = fopen (card.raw, "r");
 
-// Read data from the memory card
-fread (data, size, number, inptr);
+    // Read data from the memory card
+    fread (data, size, number, inptr);
 
-// Check if file is JPEG
-int buffer[];
-if (buffer[0] == 0xff)
-{
-    if (buffer[1] == 0xd8)
+    // Check if file is JPEG
+    int buffer[];
+    if (buffer[0] == 0xff)
     {
-        if (buffer[2] == 0xff)
+        if (buffer[1] == 0xd8)
         {
-           if ((buffer[3] && 0xf0) == 0xe0)
-           {
-            printf("file is JPEG");
-            // Make a new JPEG file
-            sprintf(filename, "%03i.jpg", 2);
-            // Open a new file with the new file name and write the data to it
-            FILE *img = fopen(filename, "w");
-            fwrite (data, size, number, outputr)
-           }
+            if (buffer[2] == 0xff)
+            {
+            if ((buffer[3] && 0xf0) == 0xe0)
+            {
+                printf("file is JPEG");
+                // Make a new JPEG file
+                sprintf(filename, "%03i.jpg", 2);
+                // Open a new file with the new file name and write the data to it
+                FILE *img = fopen(filename, "w");
+                fwrite (data, size, number, outputr)
+            }
+            }
         }
     }
-}
 
 return 1;
 
