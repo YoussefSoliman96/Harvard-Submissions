92003a16c7780a089133fec6bd92f4d1140994ea
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-08-30 07:20:42 +0000

    Wed, Aug 30, 2023, 9:20 AM +02:00

--- recover/recover.c
+++ recover/recover.c
@@ -3,10 +3,28 @@
 
 int main(int argc, char *argv[])
 {
- // Open memory card
- FILE *f = fopen (card.raw, "r");
+// Open memory card
+FILE *f = fopen (card.raw, "r");
 
- // Read data from the memory card
- fread (data, size, number, inptr);
- 
+// Read data from the memory card
+fread (data, size, number, inptr);
+
+// Check if file is JPEG
+int buffer[];
+if (buffer[0] == 0xff)
+{
+    if (buffer[1] == 0xd8)
+    {
+        if (buffer[2] == 0xff)
+        {
+            for (int i = 0; i < 16; i++)
+            {
+                if (buffer[3] == 0xe(i))
+                
+            }
+        }
+    }
+}
+
+
 }
