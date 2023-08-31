97c0f76c0e177cbdc35bd6b9e970f46568d3aca7
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-08-31 08:29:42 +0000

    Thu, Aug 31, 2023, 10:29 AM +02:00

--- CS50P/faces/faces.py
+++ CS50P/faces/faces.py
@@ -1,3 +1,10 @@
 def convert (n)
 ....if n = :) : replace(':)', '🙂')
-....if n = :() : replace(':)', '🙁')
+....if n = :( : replace(':)', '🙁')
+
+def main()
+x = input()
+convert(x)
+print(x)
+
+main()
