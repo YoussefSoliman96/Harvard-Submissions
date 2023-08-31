d3147eb793200693bcab1f47d0557870148b78c1
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-08-31 15:26:32 +0000

    Thu, Aug 31, 2023, 5:26 PM +02:00

--- CS50P/deep.py
+++ CS50P/deep.py
@@ -1,6 +1,7 @@
 x = input("What the answer to the Great Question of Life, the Universe and Everything: ")
 y = str.casefold(x)
-match y:
+z = str.translate(y)
+match z:
     case "42" | "forty-two" | "forty two":
         print("Yes")
     case _:
