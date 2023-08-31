1ff0d1ee3a7c21c45f73a0e7533d08fe9242eb7e
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-08-31 19:21:59 +0000

    Thu, Aug 31, 2023, 9:21 PM +02:00

--- CS50P/extensions/extensions.py
+++ CS50P/extensions/extensions.py
@@ -1,18 +1,1 @@
-x = input("Filename: ").lower().strip()
-if ".gif" in x:
-    print("image/gif")
-elif ".jpg" in x:
-    print("image/jpeg")
-elif ".jpeg" in x:
-    print("image/jpeg")
-elif ".png" in x:
-    print("image/png")
-elif ".pdf" in x:
-    print("application/pdf")
-elif ".txt" in x:
-    print("text/plain")
-elif ".zip" in x:
-    print("application/zip")
-
-else:
-    print("application/octet-stream")
+x = input("File name: ")
