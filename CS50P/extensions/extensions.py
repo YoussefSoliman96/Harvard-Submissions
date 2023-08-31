43ab1a16a4e9e3797ca49a28e8541ba941124d9f
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-08-31 20:03:08 +0000

    Thu, Aug 31, 2023, 10:03 PM +02:00

--- CS50P/extensions/extensions.py
+++ CS50P/extensions/extensions.py
@@ -1,7 +1,8 @@
 x = str.casefold(input("File name: "))
 y = x.split(".", 1)
-extension = y[1]
-name = y[0].strip()
+z = y.strip()
+extension = z[1]
+name = z[0]
 match extension:
     case "gif":
         print("image/" + extension)
