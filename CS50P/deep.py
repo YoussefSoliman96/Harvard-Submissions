d2d94c04560ad90c7259fee768ffa41c72654cf0
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-08-31 15:18:31 +0000

    Thu, Aug 31, 2023, 5:18 PM +02:00

--- CS50P/deep.py
+++ CS50P/deep.py
@@ -1,3 +1,4 @@
 x = input("What the answer to the Great Question of Life, the Universe and Everything: ")
-if x == 42 or x == forty-two:
-    print("Yes")
+match x:
+    case "42" | "forty-two" | "forty two":
+        print("Yes")
