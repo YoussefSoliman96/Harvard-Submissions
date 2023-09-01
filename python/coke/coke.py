3ce20493d0e4efaded5cd598982824f8e022508e
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-01 16:11:47 +0000

    Fri, Sep 1, 2023, 6:11 PM +02:00

--- python/coke/coke.py
+++ python/coke/coke.py
@@ -1,17 +1,17 @@
 def main():
     print("Amount Due: 50")
-    # Take user input
-    x = input("Insert Coin: ")
     # Calculate change
     change = calculate_change(x)
     print("Change Owed = 0")
 
 
 def calculate_change(x):
+    x = input("Insert Coin: ")
     if int(x) < 50:
         due = 50 - int(x)
-        print(f"Amount Due: {due})
-        
+        print(f"Amount Due: {due}")
+        calculate_change(x)
+    else:
 
 
 
