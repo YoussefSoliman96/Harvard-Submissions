7206cfbc2e44669fd5419572f884d4a5dffcf95b
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-01 13:00:12 +0000

    Fri, Sep 1, 2023, 3:00 PM +02:00

--- CS50P/extensions/extensions.py
+++ CS50P/extensions/extensions.py
@@ -4,19 +4,5 @@ z = y.strip
 extension = y[1]
 name = y[0].strip()
 match extension:
-    case "gif":
+    case ".gif" in filename:
         print("image/" + extension)
-    case "jpg":
-        print("image/" + extension)
-    case "jpeg":
-        print("image/" + extension)
-    case "png":
-        print("image/" + extension)
-    case "pdf":
-        print("application/" + extension)
-    case "txt":
-        print("text/" + name)
-    case "zip":
-        print("application/" + extension)
-    case _:
-        print("application/octet-stream")
