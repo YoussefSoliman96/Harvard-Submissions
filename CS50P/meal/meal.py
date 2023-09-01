6284ee597ba08dd608f1b8e31c013e18321ba4bd
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-01 13:55:07 +0000

    Fri, Sep 1, 2023, 3:55 PM +02:00

--- CS50P/meal/meal.py
+++ CS50P/meal/meal.py
@@ -1,7 +1,7 @@
 x, y = input("What time is it: ").split(":")
-if x = "7"
+if x >= 7 and x <=8:
     print("breakfast time")
-elif x = "12"
+elif x >= 12 and x <=13:
     print("lunch time")
-elif x = "18"
+elif x >= 18 and x <=19:
     print("dinner time")
