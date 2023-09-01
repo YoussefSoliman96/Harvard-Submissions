1b3783474a97b4aa71577049a1ee7e3fc353d7f7
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-01 17:22:49 +0000

    Fri, Sep 1, 2023, 7:22 PM +02:00

--- python/twttr/twttr.py
+++ python/twttr/twttr.py
@@ -1,5 +1,7 @@
 x = input("Input: ")
 for c in x:
     if c == "a" or c == "e" or c == "i" or c == "o" or c == "u":
-        y = x.replace("c", "")
+        y = c.replace("c", "")
         print(y)
+    else:
+        print(c)
