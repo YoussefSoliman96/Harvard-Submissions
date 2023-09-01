bc6a5226b9927e82f7d5b511a3016dac7d4eac08
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-01 17:08:50 +0000

    Fri, Sep 1, 2023, 7:08 PM +02:00

--- python/coke/coke.py
+++ python/coke/coke.py
@@ -1,11 +1,13 @@
 due = 50
 
 while due > 0:
-    print("Amount Due: ", due)
-    x = int(input("Insert Coin: "))
+    print("Amount Due:", due)
+    x = int(input("Insert Coin:"))
     if x == 5 or x == 10 or x == 25:
         due = due - x
         if due <= 0:
             change = abs(due)
-            print(f"Change Owed: {change}")
+            print(f"Change Owed:{change}")
             break
+
+
