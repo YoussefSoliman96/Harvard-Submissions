324fbf9d1ebfcff8d1b7dc6826de6e15008c89c2
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-01 16:44:43 +0000

    Fri, Sep 1, 2023, 6:44 PM +02:00

--- python/coke/coke.py
+++ python/coke/coke.py
@@ -4,6 +4,6 @@ if due > 0:
     print(f"Amount Due: {due}")
     x = input("Insert Coin: ")
     due -= int(x)
-
-change = abs(due)
-print(f"Change Owed: {change}")
+else:
+    change = abs(due)
+    print(f"Change Owed: {change}")
