40b0a527a8b3a07a23fd7387ee86a6f9a01d8da5
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-01 15:26:34 +0000

    Fri, Sep 1, 2023, 5:26 PM +02:00

--- python/camel/camel.py
+++ python/camel/camel.py
@@ -3,6 +3,7 @@ for c in x:
     if c.isupper() == True:
         y = "_" + c.lower()
         c = y
-        print(c, end = "")
+        print(c, end="")
     else:
-        print(c, end = "")
+        print(c, end="")
+print()
