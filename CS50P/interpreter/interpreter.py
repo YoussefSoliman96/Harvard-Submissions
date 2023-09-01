b5e02c16a176f68278551711ae16d782f2c9e550
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-01 13:34:01 +0000

    Fri, Sep 1, 2023, 3:34 PM +02:00

--- CS50P/interpreter/interpreter.py
+++ CS50P/interpreter/interpreter.py
@@ -2,13 +2,13 @@ input = input("Expression: ")
 x, y, z = input.split()
 nx = float(x)
 nz = float(z)
-match y:
-    case "+":
-        output = nx + nz
-    case "-":
-        output = nx - nz
-    case "*":
-        output = nx * nz
-    case "/":
-        output = nx / nz
-        print(output)
+if y == "+":
+    output = nx + nz
+elif y == "-":
+    output = nx - nz
+elif y == "*":
+    output = nx * nz
+elif y == "/":
+    output = nx / nz
+    print(output)
+
