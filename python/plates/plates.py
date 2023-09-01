0bc09783f7659ef5c1122bdd56eb0e2a4265c167
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-01 19:17:52 +0000

    Fri, Sep 1, 2023, 9:17 PM +02:00

--- python/plates/plates.py
+++ python/plates/plates.py
@@ -10,7 +10,10 @@ def is_valid(plate):
     x = len(plate)
     letters = plate[0 : int(x/2)]
     numbers = plate[int(x/2) : x]
-    if letters > 2 and letters + numbers <=6 and letters.isaplha() == True
+    if len(letters) > 2 and x <=6 and letters.isaplha() == True:
+        return True
+    else:
+        return False
 
 
 
