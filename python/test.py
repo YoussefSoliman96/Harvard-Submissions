1d6d53e7290906e3cfb62fedfa9003b7babdf099
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-01 19:01:07 +0000

    Fri, Sep 1, 2023, 9:01 PM +02:00

--- python/test.py
+++ python/test.py
@@ -1,5 +1,5 @@
 plate = input("Plate: ")
-for i in plate[i]:
-    letters = plate[0:i/2]
-    numbers = plate[i/2:i]
+for i in plate:
+    letters = plate[0:int(i)/2]
+    numbers = plate[int(i)/2:i]
     print(letters, numbers)
