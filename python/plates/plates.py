3c3ef82ac85a295806c81b366cf7dfecacace9c4
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-01 19:33:41 +0000

    Fri, Sep 1, 2023, 9:33 PM +02:00

--- python/plates/plates.py
+++ python/plates/plates.py
@@ -9,8 +9,10 @@ def main():
 def is_valid(s):
     if len(s) < 2 and x > 6 or s[0].isalpha() == False or s[1].isalpha == False:
         return False
-    for _ in s:
-        if 
+    for i in range(len(s)):
+        if s[i].isalpha() == Flase:
+            if s[i] == "0":
+                return False
 
 
 
