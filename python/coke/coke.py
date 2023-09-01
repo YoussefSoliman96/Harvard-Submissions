fbd2253a39e4f937c51c8136c4ca329cd440fbcd
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-01 16:23:53 +0000

    Fri, Sep 1, 2023, 6:23 PM +02:00

--- python/coke/coke.py
+++ python/coke/coke.py
@@ -1,10 +1,10 @@
-
+due = 50
 print("Amount Due: 50")
 x = input("Insert Coin: ")
 
-while int(x) < 50:
-    x = 50 - int(x)
-    x =
+while int(x) < due:
+    x = due - int(x)
+    due = due - x
     print(f"Amount Due: {due}")
     y = input("Insert Coin: ")
     if int(x) >= 50:
