a2e6799158c853d1eb8bfae8add65dcd26b4ce7f
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-01 19:06:15 +0000

    Fri, Sep 1, 2023, 9:06 PM +02:00

--- python/test.py
+++ python/test.py
@@ -1,5 +1,6 @@
 plate = input("Plate: ")
-x = int(len(plate))
-letters = plate[0:x/2]
-numbers = plate[x/2/x]
+x = len(plate)
+print(x)
+letters = plate[0 : x/2]
+numbers = plate[x/2 : x]
 print(letters, numbers)
