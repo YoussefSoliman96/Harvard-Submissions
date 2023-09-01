eab146b8c0174f139797ca86949ae50f074ddcc5
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-01 15:48:39 +0000

    Fri, Sep 1, 2023, 5:48 PM +02:00

--- python/coke/coke.py
+++ python/coke/coke.py
@@ -1,2 +1,16 @@
+def main():
 print("Amount Due: 50")
+change = calculate_change(coins)
 x = input("Insert Coin: ")
+
+
+
+def calculate_change(coins):
+    change = x - 50
+    if change > 0
+        return change
+    
+
+
+
+    main()
