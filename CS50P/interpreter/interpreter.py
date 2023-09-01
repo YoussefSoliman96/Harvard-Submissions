33676c023fda3656df2300151d7955aec8d5e215
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-01 13:26:03 +0000

    Fri, Sep 1, 2023, 3:26 PM +02:00

--- CS50P/interpreter/interpreter.py
+++ CS50P/interpreter/interpreter.py
@@ -14,4 +14,19 @@ match y:
 print(output)
 
 x, y, z = input.split()
-output = (x + y + z)
+nx = float(x)
+nz = float(z)
+match y:
+    case "+":
+        output = nx + nz
+    case "-":
+        output = nx - nz
+    case "*":
+        output = nx * nz
+    case "/":
+        output = nx / nz
+print(output)
+
+x, y, z = input.split()
+nx = float(x)
+nz = float(z)
