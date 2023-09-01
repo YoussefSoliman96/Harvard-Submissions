6bf422db012c0be7b171dfd54058d3c792c1af9e
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-01 13:11:24 +0000

    Fri, Sep 1, 2023, 3:11 PM +02:00

--- CS50P/extensions/extensions.py
+++ CS50P/extensions/extensions.py
@@ -1,8 +1,19 @@
-x = str.casefold(input("File name: "))
-y = x.split(".", 1)
-z = y.strip
-extension = y[1]
-name = y[0].strip()
-match extension:
-    case ".gif" in filename:
-        print("image/" + extension)
+x = input("Filename: ").lower().strip()
+print(x)
+if ".gif" in x:
+    print("images/gif")
+elif ".jpg" in x:
+    print("images/jpg")
+elif ".jpeg" in x:
+    print("images/jpeg")
+elif ".png" in x:
+    print("images/png")
+elif ".pdf" in x:
+    print("application/pdf")
+elif ".txt" in x:
+    print("text/txt")
+elif ".zip" in x:
+    print("application/zip")
+
+else:
+    print("application/octet-stream")
