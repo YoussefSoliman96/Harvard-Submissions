a5acca1388b802ff1ee1b835bd80bcf36d93f2b9
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-01 18:52:57 +0000

    Fri, Sep 1, 2023, 8:52 PM +02:00

--- python/plates.py
+++ python/plates.py
@@ -1,2 +1,13 @@
-x = input("Plates: ")
-for c 
+def main():
+    plate = input("Plate: ")
+    if is_valid(plate):
+        print("Valid")
+    else:
+        print("Invalid")
+
+
+def is_valid(s):
+    ...
+
+
+main()
