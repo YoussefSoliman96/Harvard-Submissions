92b2282768623ea3ec830db6c12d71d6f60ff0d8
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-02 16:39:35 +0000

    Sat, Sep 2, 2023, 6:39 PM +02:00

--- python/outdated/outdated.py
+++ python/outdated/outdated.py
@@ -12,3 +12,16 @@ months = [
     "November",
     "December"
 ]
+
+# Loop forever until user prompts a valid input
+while True:
+    try:
+        # Prompt the user for input
+        date = input("Date: ")
+        x = str.split(date, "/")
+        day = x[1]
+        month = x[0]
+        year = x[2]
+    except ValueError:
+        break
+print (day, month, year)
