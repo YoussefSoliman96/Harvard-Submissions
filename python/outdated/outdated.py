93796ee3ce502e2fbd7e6dd13b1ea6a6004c7e82
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-02 17:41:54 +0000

    Sat, Sep 2, 2023, 7:41 PM +02:00

--- python/outdated/outdated.py
+++ python/outdated/outdated.py
@@ -30,15 +30,15 @@ while True:
     except:
         try:
             # Split the date by spaces
-            y = date.split(" ")
-            nday = y[1]
-            nmonth = y[0]
-            nyear = y[2]
+            x = date.split(" ")
+            day = x[1]
+            month = x[0]
+            year = x[2]
             # Remove comma from date
-            day = nday.replace(",", " ")
+            day = day.replace(",", " ")
             # Identify the month
             for i in range(len(months)):
-                if nmonth == months[i]:
+                if month == months[i]:
                     month = i + 1
             # Check validity of date based on count
             if 1 <= int(day) <= 31 and 1 <= int(month) <= 12:
