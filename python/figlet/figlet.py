2de9c4fc599689316dda559c9e0ec0a756b21ce7
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-02 14:04:24 +0000

    Sat, Sep 2, 2023, 4:04 PM +02:00

--- python/figlet/figlet.py
+++ python/figlet/figlet.py
@@ -5,16 +5,16 @@ from pyfiglet import Figlet
 figlet = Figlet()
 # Checking whether to print a random font or a certain font that the user requested
 if len(sys.argv) == 1:
-    random = True
+    randomn = True
 elif len(sys.argv) == 3 and (sys.argv[1] == "-f" or sys.argv[1] == "--font"):
-    random = False
+    randomn = False
 else:
     sys.exit(1)
 
 # Import fonts
 figlet.getFonts()
 # Set the font after importing it and setting it's name to f
-if random == False:
+if randomn == False:
     try:
         figlet.setFont(font=sys.argv[2])
     except:
