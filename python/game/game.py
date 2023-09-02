9ba908be7f0bb6d6a9f01fae9807ad6458522a67
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-02 19:18:20 +0000

    Sat, Sep 2, 2023, 9:18 PM +02:00

--- python/game/game.py
+++ python/game/game.py
@@ -2,17 +2,20 @@ import random
 # Loop forever until the user prompts a positive level
 while True:
     n = input("Level: ")
-    guess = input("Guess: ")
     try:
         if int(n) > 0:
             level = random.randint(1, int(n))
-            if int(guess) < int(level):
-                print("Too small!")
-            elif int(guess) > int(level):
-                print("Too large!")
-            else:
-                print("Just right!")
-            break
+            try:
+                guess = input("Guess: ")
+                if int(guess) < int(level):
+                    print("Too small!")
+                elif int(guess) > int(level):
+                    print("Too large!")
+                else:
+                    print("Just right!")
+                    break
+            except ValueError:
+                break
     except ValueError:
         print("Input is not a number")
 
