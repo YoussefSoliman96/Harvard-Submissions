84617b964ee070065d4289e9070bfaa13bb600c9
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-02 14:30:38 +0000

    Sat, Sep 2, 2023, 4:30 PM +02:00

---
+++ python/adieu/adieu.py
@@ -0,0 +1,15 @@
+# Make a list to keep all user inputs
+names = []
+# Take user input
+# Loop forever and re-prompt the user for input
+while True:
+    try:
+        name = input("Name: ")
+        names.append(name)
+# When user clicks ctrl-d, print an empty space and break
+    except EOFERROR:
+        print("")
+        break
+
+# print the output starting with Adieu, adieu, to
+print("Adieu, adieu, to ")
