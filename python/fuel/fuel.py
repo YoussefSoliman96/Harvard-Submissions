0b7c238555dae3dce31abd05106925ea4435d842
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-02 15:05:54 +0000

    Sat, Sep 2, 2023, 5:05 PM +02:00

--- python/fuel/fuel.py
+++ python/fuel/fuel.py
@@ -4,13 +4,12 @@ while True:
     fraction = input("Fraction: ")
     try:
         x = fraction.split("/")
-        
-
+        result = round((int(x[0]) / int(x[1]))*100)
+        if result <= 1:
+            break
     except ValueError:
         pass
     except ZeroDivisionError:
         pass
 
-
-    result = (int(x[0]) / int(x[1]))*100
     print(f"%{result}")
