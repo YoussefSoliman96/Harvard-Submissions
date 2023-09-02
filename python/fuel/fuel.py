be68a78012d60754400c37b0cebf2d4783523c1e
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-02 15:02:09 +0000

    Sat, Sep 2, 2023, 5:02 PM +02:00

--- python/fuel/fuel.py
+++ python/fuel/fuel.py
@@ -1,11 +1,13 @@
 # Take user input
 # Loop forever until user input is in the right format (True)
-try:
-    fraction = input("Fraction: ")
-except ValueError:
-    print("")
-except ZeroDivisionError:
-    print("")
-x = fraction.split("/")
-result = (int(x[0]) / int(x[1]))*100
-print(f"%{result}")
+while True:
+    try:
+        fraction = input("Fraction: ")
+    except ValueError:
+        pass
+    except ZeroDivisionError:
+        pass
+
+    x = fraction.split("/")
+    result = (int(x[0]) / int(x[1]))*100
+    print(f"%{result}")
