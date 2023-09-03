6248569b2b4eba1063afeb0accccd85162f415b5
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-03 07:56:46 +0000

    Sun, Sep 3, 2023, 9:56 AM +02:00

--- python/professor/professor.py
+++ python/professor/professor.py
@@ -3,6 +3,7 @@ while True:
         n = int(input("Level: "))
         try:
             if n >= 1 and n < 4:
+                random.choice
                 break
         except ValueError:
             print("Input is not an integer ")
