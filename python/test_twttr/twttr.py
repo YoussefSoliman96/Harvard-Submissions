80ad03fc245e75f5c149f6ea57cab8b4abc62394
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-03 22:03:24 +0000

    Mon, Sep 4, 2023, 12:03 AM +02:00

--- python/test_twttr/twttr.py
+++ python/test_twttr/twttr.py
@@ -9,7 +9,7 @@ def shorten(word):
     new_word = ""
     for c in word:
         if c == "a" or c == "e" or c == "i" or c == "o" or c == "u":
-            v = c.replace("c", "")
+            v = c.replace(c, "")
             new_word += v
         else:
             new_word += c
