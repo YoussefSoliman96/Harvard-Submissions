8c312fe2a44ff7217bb04df03f56158eb6c80ca9
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-03 13:53:43 +0000

    Sun, Sep 3, 2023, 3:53 PM +02:00

--- python/professor/professor.py
+++ python/professor/professor.py
@@ -1,24 +1,21 @@
 import random
-n = int(input("Level: "))
-count = 0
-wrong_count = 0
-while count < 3
+def main():
+    ...
+
+
+def get_level():
+while True:
+    n = int(input("Level: "))
+    try:
+        
         x = random.randint(1, 10)
         y = random.randint(1,10)
-        result = x + y
-        print(f"{x} + {y} = ",end=" ")
-        try:
-            if n >= 1 and n < 4:
-                while True:
-                    answer = int(input(""))
-                    if answer == result or wrong_count == 2:
-                        break
-                    elif answer != result:
-                        print("EEE")
-                        print(f"{x} + {y} = ",end=" ")
-                        wrong_count += 1
+    except:
 
 
-            count += 1
-        except ValueError:
-            print("Input is not an integer ")
+def generate_integer(level):
+    ...
+
+
+if __name__ == "__main__":
+    main()
