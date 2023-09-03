71bb97760e1e70ccee9b1cc946e9c1b85795705a
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-03 22:02:35 +0000

    Mon, Sep 4, 2023, 12:02 AM +02:00

--- python/test_twttr/twttr.py
+++ python/test_twttr/twttr.py
@@ -1,16 +1,19 @@
 def main():
     word =  input("Input: ").lower()
-    new_word = shorten(word)
-    print("Output: " + new_word)
+    new_message = shorten(word)
+    print("Output: " + new_message)
 
 
 
 def shorten(word):
+    new_word = ""
     for c in word:
         if c == "a" or c == "e" or c == "i" or c == "o" or c == "u":
-            return c.replace("c", "")
+            v = c.replace("c", "")
+            new_word += v
         else:
-            return c
+            new_word += c
+    return new_word
 
 if __name__ == "__main__":
     main()
