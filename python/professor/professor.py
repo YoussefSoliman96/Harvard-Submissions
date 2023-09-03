cd58b453edb357a1e525196f076ad1b94256e251
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-03 09:32:41 +0000

    Sun, Sep 3, 2023, 11:32 AM +02:00

--- python/professor/professor.py
+++ python/professor/professor.py
@@ -2,10 +2,34 @@ import random
 
 def main():
     n = get_level()
+    generate_integer(n)
+
+
+
+def get_level():
+    while True:
+            n = input("Level: ")
+            try:
+                if int(n) > 0 and int(n) < 4:
+                    break
+            except ValueError:
+                print("Input is not an integer ")
+                pass
+
+def generate_integer(n):
     count = 0
     wrong_count = 0
     while count < 10:
-            generate_integer(n)
+            if n == 1:
+                x = random.randint(1, 9)
+                y = random.randint(1, 9)
+            elif n == 2:
+                x = random.randint(10, 99)
+                y = random.randint(10, 99)
+            else:
+                x = random.randint(100, 999)
+                y = random.randint(10, 999)
+
             result = x + y
             print(f"{x} + {y} = ",end=" ")
             try:
@@ -20,29 +44,6 @@ def main():
                 count += 1
             except ValueError:
                 print("Input is not an integer ")
-
-
-def get_level():
-    while True:
-            n = input("Level: ")
-            try:
-                if int(n) > 0 and int(n) < 4:
-                    break
-            except ValueError:
-                print("Input is not an integer ")
-                pass
-
-def generate_integer(n):
-    if n == 1:
-        x = random.randint(1, 9)
-        y = random.randint(1, 9)
-    elif n == 2:
-        x = random.randint(10, 99)
-        y = random.randint(10, 99)
-    else:
-        x = random.randint(100, 999)
-        y = random.randint(10, 999)
-    return x,y
 
 
 if __name__ == "__main__":
