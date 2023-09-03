f1d3f614e17cc85e9459fbc5af0569c0aa4de59a
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-03 21:53:00 +0000

    Sun, Sep 3, 2023, 11:53 PM +02:00

--- python/test_twttr/twttr.py
+++ python/test_twttr/twttr.py
@@ -1,15 +1,15 @@
 def main():
     word =  input("Input: ").lower()
     new_word = shorten(word)
-    print(new_word)
 
 
 def shorten(word):
     for c in word:
         if c == "a" or c == "e" or c == "i" or c == "o" or c == "u":
-            word = c.replace("c", "")
+            v = c.replace("c", "")
+            print(v, end="")
         else:
-            
+            print(c, end="")
     return word
 
 
