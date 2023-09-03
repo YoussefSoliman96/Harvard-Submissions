b03295516335fade00fb7fd94eab848a649fdb74
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-03 09:02:34 +0000

    Sun, Sep 3, 2023, 11:02 AM +02:00

--- python/professor/professor.py
+++ python/professor/professor.py
@@ -33,24 +33,10 @@ def get_level():
                 print("Input is not an integer ")
                 pass
 
-def generate_integer(level):
-    while True:
-            x = random.randint(1, 10)
-            y = random.randint(1,10)
-            result = x + y
-            print(f"{x} + {y} = ",end=" ")
-            try:
-                while True:
-                    answer = int(input(""))
-                    if answer == result or wrong_count == 2:
-                        break
-                    elif answer != result:
-                        print("EEE")
-                        print(f"{x} + {y} = ",end=" ")
-                        wrong_count += 1
-                count += 1
-            except ValueError:
-                print("Input is not an integer ")
+def generate_integer(n):
+    digits = n
+    x = random.randint(1, d)
+    y = random.randint(1, d)
 
 
 if __name__ == "__main__":
