6534757f9953d2943c4ce8d31e865f2c3975e6a9
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-03 07:59:43 +0000

    Sun, Sep 3, 2023, 9:59 AM +02:00

--- python/professor/professor.py
+++ python/professor/professor.py
@@ -3,7 +3,10 @@ while True:
         n = int(input("Level: "))
         try:
             if n >= 1 and n < 4:
-                random.choice
+                x = random.randint(1, 10)
+                y = random.randint(1,10)
+                result = x + y
+                print(x, y, result)
                 break
         except ValueError:
             print("Input is not an integer ")
