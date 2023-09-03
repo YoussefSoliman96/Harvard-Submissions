28b1619fa46d7738af1b6f213e8a27a28ecabae2
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-03 08:36:16 +0000

    Sun, Sep 3, 2023, 10:36 AM +02:00

--- python/professor/professor.py
+++ python/professor/professor.py
@@ -7,16 +7,14 @@ while True:
                 y = random.randint(1,10)
                 result = x + y
                 print(f"{x} + {y} = ",end=" ")
+                answer = int(input(""))
+                try:
+                if answer == result:
+                    break
+                elif answer != result:
+                    print("EEE")
+                except ValueError:
+                    print("Input should be a positive integer")
                 break
         except ValueError:
             print("Input is not an integer ")
-
-while True:
-        answer = int(input(""))
-        try:
-            if answer == result:
-                 break
-            elif answer != result:
-                 print("EEE")
-        except ValueError:
-            print("Input should be a positive integer")
