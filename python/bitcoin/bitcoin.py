8bb09658656896db4c6aca06c9e7cf0a269d95bf
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-03 15:24:44 +0000

    Sun, Sep 3, 2023, 5:24 PM +02:00

--- python/bitcoin/bitcoin.py
+++ python/bitcoin/bitcoin.py
@@ -7,4 +7,7 @@ if len(sys.argv) > 2:
     sys.exit(print("Too many arguments"))
 else:
     r = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
-    print(r.json())
+    try:
+        print(r.json())
+    except requests.RequestException:
+        sys.exit(1)
