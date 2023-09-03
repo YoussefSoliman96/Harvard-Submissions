e7e01d0eca3f266fc9cb5ef9b704581e6a75e780
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-03 22:39:29 +0000

    Mon, Sep 4, 2023, 12:39 AM +02:00

--- python/test_twttr/test_twttr.py
+++ python/test_twttr/test_twttr.py
@@ -1,14 +1,20 @@
 from twttr import shorten
 
 def main():
-    test_shorten()
+    test_case()
+    test_numbers()
+    test_punctuation()
 
-def test_shorten():
+def test_case():
     assert shorten("twitter") == "twttr"
     assert shorten("TWITTER") == "TWTTR"
     assert shorten("TwItTeR") == "TwtTR"
 
+def test_numbers():
+    assert shorten("123") == "123"
 
+def test_punctuation():
+    assert shorten("!.?") == "!.?"
 
 if __name__ == "__main__":
     main()
