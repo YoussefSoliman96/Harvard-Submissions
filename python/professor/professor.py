094b4f8a37ddd46a188ce054fe4ba91dcef9df27
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-03 08:42:15 +0000

    Sun, Sep 3, 2023, 10:42 AM +02:00

--- python/professor/professor.py
+++ python/professor/professor.py
@@ -1,12 +1,12 @@
 import random
+n = int(input("Level: "))
 while True:
-        n = int(input("Level: "))
+        x = random.randint(1, 10)
+        y = random.randint(1,10)
+        result = x + y
+        print(f"{x} + {y} = ",end=" ")
         try:
             if n >= 1 and n < 4:
-                x = random.randint(1, 10)
-                y = random.randint(1,10)
-                result = x + y
-                print(f"{x} + {y} = ",end=" ")
                 while True:
                     answer = int(input(""))
                     if answer == result:
