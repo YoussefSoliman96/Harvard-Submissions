b12c8ca36750ef7a9e105608746684d47bbacb77
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-03 13:56:20 +0000

    Sun, Sep 3, 2023, 3:56 PM +02:00

--- python/professor/professor.py
+++ python/professor/professor.py
@@ -1,16 +1,17 @@
 import random
 def main():
-    ...
+    get_level()
 
 
 def get_level():
-while True:
-    n = int(input("Level: "))
-    try:
-        
-        x = random.randint(1, 10)
-        y = random.randint(1,10)
-    except:
+    while True:
+        n = int(input("Level: "))
+        try:
+            if n == 1 or n == 2 or n == 3:
+                break
+        except:
+            pass
+        return n
 
 
 def generate_integer(level):
