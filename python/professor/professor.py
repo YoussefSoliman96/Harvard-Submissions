a21e193119f36de1db1d6e8278ccf7db844a421f
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-03 09:40:37 +0000

    Sun, Sep 3, 2023, 11:40 AM +02:00

--- python/professor/professor.py
+++ python/professor/professor.py
@@ -1,50 +1,24 @@
 import random
-
-def main():
-    level = get_level()
-    generate_integer(level)
-
-
-
-def get_level():
-    while True:
-            n = input("Level: ")
-            try:
-                if int(n) > 0 and int(n) < 4:
-                    break
-            except ValueError:
-                print("Input is not an integer ")
-                pass
-
-def generate_integer(n):
-    count = 0
-    wrong_count = 0
-    while count < 10:
-            if n == 1:
-                x = random.randint(1, 9)
-                y = random.randint(1, 9)
-            elif n == 2:
-                x = random.randint(10, 99)
-                y = random.randint(10, 99)
-            else:
-                x = random.randint(100, 999)
-                y = random.randint(10, 999)
-
+n = int(input("Level: "))
+count = 0
+wrong_count = 0
+while count < 3:
+        x = random.randint(1, 10)
+        y = random.randint(1,10)
         result = x + y
         print(f"{x} + {y} = ",end=" ")
         try:
-            while True:
-                answer = int(input(""))
-                if answer == result or wrong_count == 2:
-                    break
-                elif answer != result:
-                    print("EEE")
-                    print(f"{x} + {y} = ",end=" ")
-                    wrong_count += 1
+            if n >= 1 and n < 4:
+                while True:
+                    answer = int(input(""))
+                    if answer == result or wrong_count == 2:
+                        break
+                    elif answer != result:
+                        print("EEE")
+
+                        wrong_count += 1
+
+
             count += 1
         except ValueError:
             print("Input is not an integer ")
-
-
-if __name__ == "__main__":
-    main()
