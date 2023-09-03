f377903e72aeaebfbca7311b6e7750ebc6fab0ca
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-03 15:30:47 +0000

    Sun, Sep 3, 2023, 5:30 PM +02:00

--- python/bitcoin/bitcoin.py
+++ python/bitcoin/bitcoin.py
@@ -7,6 +7,7 @@ if len(sys.argv) > 2:
     sys.exit(print("Too many arguments"))
 else:
     r = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
+    bitcoin_price = r.json()["bpi"]["USD"]["rate_float"]
     try:
         print(r.json()["bpi"]["USD"]["rate_float"])
     except requests.RequestException:
