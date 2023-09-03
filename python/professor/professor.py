809c1be0896ebd835bb7f42fb463859bef748567
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-03 07:26:55 +0000

    Sun, Sep 3, 2023, 9:26 AM +02:00

--- python/professor/professor.py
+++ python/professor/professor.py
@@ -1,7 +1,8 @@
 
 while True:
-    level = input("Level: ")
+    n = input("Level: ")
         try:
-
+            if n >= 1 and n < 4:
+            break
         except ValueError:
-            
+            print("Input is not an integer ")
