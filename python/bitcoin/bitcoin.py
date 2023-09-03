a02f5c65789ae60e8ffc4d5e58b5da632996e080
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-03 15:42:24 +0000

    Sun, Sep 3, 2023, 5:42 PM +02:00

--- python/bitcoin/bitcoin.py
+++ python/bitcoin/bitcoin.py
@@ -8,18 +8,13 @@ if len(sys.argv) > 2:
     print("Too many arguments")
     sys.exit(1)
 else:
-    try:
-        # Convert the user input to float
-        bitcoin_amount = float(sys.argv[1])
-    except:
-        sys.exit(print("Command-line argument invalid"))
+
+    # Convert the user input to float
+    bitcoin_amount = float(sys.argv[1])
     # Get the API response
     r = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
     # Get the bitcoin price
     bitcoin_price = r.json()["bpi"]["USD"]["rate_float"]
     # Get the total price of the bitcoins the user requested
     total_price = bitcoin_amount * bitcoin_price
-    try:
-        print(f"${total_price:,.4f}")
-    except requests.RequestException:
-        sys.exit(1)
+    print(f"${total_price:,.4f}")
