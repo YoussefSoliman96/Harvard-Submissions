dc9f9cae36eeaaf77b84f3cb57f1ce2d628a17fc
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-03 08:55:01 +0000

    Sun, Sep 3, 2023, 10:55 AM +02:00

--- python/professor/professor.py
+++ python/professor/professor.py
@@ -1,14 +1,15 @@
 import random
-n = int(input("Level: "))
-count = 0
-wrong_count = 0
-while count < 10:
-        x = random.randint(1, 10)
-        y = random.randint(1,10)
-        result = x + y
-        print(f"{x} + {y} = ",end=" ")
-        try:
-            if n >= 1 and n < 4:
+
+def main():
+    get_level()
+    count = 0
+    wrong_count = 0
+    while count < 10:
+            x = random.randint(1, 10)
+            y = random.randint(1,10)
+            result = x + y
+            print(f"{x} + {y} = ",end=" ")
+            try:
                 while True:
                     answer = int(input(""))
                     if answer == result or wrong_count == 2:
@@ -17,8 +18,24 @@ while count < 10:
                         print("EEE")
                         print(f"{x} + {y} = ",end=" ")
                         wrong_count += 1
+                count += 1
+            except ValueError:
+                print("Input is not an integer ")
 
 
-            count += 1
-        except ValueError:
-            print("Input is not an integer ")
+def get_level():
+    while True:
+            n = input("Level: ")
+            try:
+                if int(n) > 0:
+                    break
+            except ValueError:
+                print("Input is not an integer ")
+                pass
+
+def generate_integer(level):
+    ...
+
+
+if __name__ == "__main__":
+    main()
