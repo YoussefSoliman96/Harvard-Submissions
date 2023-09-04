9c53f9bc33cf481fadad7d7a53ce96a83b88e1be
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-04 14:00:29 +0000

    Mon, Sep 4, 2023, 4:00 PM +02:00

--- python/test_fuel/test_fuel.py
+++ python/test_fuel/test_fuel.py
@@ -2,9 +2,11 @@ from fuel import convert
 from fuel import gauge
 
 def main():
+    test_convert()
 
 def test_convert():
-    assert convert("1/2") == 
+    assert convert("1/2") == 1/2
+    assert convert("2/1") == 
 
 
 
