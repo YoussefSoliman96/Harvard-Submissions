cfc2c7392793002e5163727dfd4bc8563ff94867
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-04 13:32:10 +0000

    Mon, Sep 4, 2023, 3:32 PM +02:00

--- python/test_plates/test_plates.py
+++ python/test_plates/test_plates.py
@@ -3,12 +3,12 @@ from plates import is_valid
 def main():
     test_valid()
 
-def test_validd():
+def test_valid():
     assert is_valid("HELLO") == True
     assert is_valid("hello") == True
     assert is_valid("CS50") == True
     assert is_valid("CS50P2") == False
     assert is_valid("HELLO, world") == False
 
-
+if __name__ == "__main__":
     main()
