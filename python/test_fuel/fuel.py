545896a86e9ebd829c53ec3e30d8a59756f6a9cb
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-04 14:28:57 +0000

    Mon, Sep 4, 2023, 4:28 PM +02:00

--- python/test_fuel/fuel.py
+++ python/test_fuel/fuel.py
@@ -10,16 +10,16 @@ def convert(fraction):
     while True:
         try:
             x = fraction.split("/")
-            result = (int(x[0]) / int(x[1]))
+            result = int((int(x[0]) / int(x[1])))
             if result <= 1:
-                p = int(result * 100)
-                return p
+                return result
         except ValueError:
             pass
         except ZeroDivisionError:
             pass
 
-def gauge(p):
+def gauge(result):
+    p = round(result * 100)
     # If percentage < 1%, print E
     if p <= 1:
         return "E"
--- python/test_fuel/test_fuel.py
+++ python/test_fuel/test_fuel.py
@@ -1,8 +1,11 @@
 from fuel import convert
 from fuel import gauge
+import pytest
 
 def main():
     test_convert()
+    test_division()
+    test_value()
 
 def test_convert():
     assert convert("1/2") == 1/2
@@ -13,9 +16,13 @@ def test_gauge():
     assert gauge("5/100") == "%5"
 
 
+def test_division():
+    with pytest.raises(ZeroDivisionError):
+        convert("1/0")
 
-
-
+def test_value():
+    with pytest.raises(ValueError):
+        convert("d/c")
 
 if __name__ == "__main__":
     main()
