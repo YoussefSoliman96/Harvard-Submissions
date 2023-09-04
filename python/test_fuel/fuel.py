56eed5ead3fb6b74d0af556ad1cc630ca58f0d81
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-04 14:22:56 +0000

    Mon, Sep 4, 2023, 4:22 PM +02:00

--- python/test_fuel/fuel.py
+++ python/test_fuel/fuel.py
@@ -12,14 +12,14 @@ def convert(fraction):
             x = fraction.split("/")
             result = (int(x[0]) / int(x[1]))
             if result <= 1:
-                return result
+                p = int(result * 100)
+                return p
         except ValueError:
             pass
         except ZeroDivisionError:
             pass
 
-def gauge(result):
-    p = round(result * 100)
+def gauge(p):
     # If percentage < 1%, print E
     if p <= 1:
         return "E"
