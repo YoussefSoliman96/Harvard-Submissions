ecb7c75b8b5077e93b318c930812acd95b0803a8
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-04 21:01:54 +0000

    Mon, Sep 4, 2023, 11:01 PM +02:00

--- python/test_fuel/test_fuel.py
+++ python/test_fuel/test_fuel.py
@@ -5,18 +5,11 @@ import pytest
 def main():
     test_convert()
     test_division()
-    test_value()
 
 def test_convert():
-    assert convert("1/2") == 50
-    assert convert("1/100") == 1
-    assert convert("99/100") == 99
-
-
-def test_gauge():
-    assert gauge(1) == "E"
-    assert gauge(99) == "F"
-    assert gauge(5) == "%5"
+    assert convert("1/2") == 50 and gauge(50) == "%50"
+    assert convert("1/100") == 1 and gauge(1) == "E"
+    assert convert("99/100") == 99 and gauge(50) == "%50"
 
 
 def test_division():
