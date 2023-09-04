b6464da1ac274b84c7ec0e355d12a6e5856763e7
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-04 13:35:31 +0000

    Mon, Sep 4, 2023, 3:35 PM +02:00

--- python/test_plates/test_plates.py
+++ python/test_plates/test_plates.py
@@ -9,6 +9,9 @@ def test_valid():
     assert is_valid("CS50") == True
     assert is_valid("CS50P2") == False
     assert is_valid("HELLO, world") == False
+    assert is_valid("50CS") == False
+    assert is_valid("HELLOOOO") == False
+    assert is_valid("CS05") == False
 
 if __name__ == "__main__":
     main()
