1397f57ee1e8d83f47d8ba7fb1787b22d6b30d91
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-04 13:24:32 +0000

    Mon, Sep 4, 2023, 3:24 PM +02:00

--- python/test_plates/plates.py
+++ python/test_plates/plates.py
@@ -1,9 +1,37 @@
 def main():
-    ...
+    plate = input("Plate: ")
+    if is_valid(plate):
+        print("Valid")
+    else:
+        print("Invalid")
 
 
 def is_valid(s):
-    ...
+    # All vanity plates must start with at least two letters
+    # Vanity plates may contain a maximum of 6 characters (letters or numbers) and a minimum of 2 characters
+    if len(s) < 2 or len(s) > 6 or s[0].isalpha() == False or s[1].isalpha == False:
+        return False
+    # Numbers cannot be used in the middle of a plate; they must come at the end
+    i = 0
+    for i in range(len(s)):
+        if s[i].isalpha() == False:
+            if s[i] == "0":
+                return False
+            else:
+                 break
+
+    digit_count = 0
+    for c in s:
+        if(c.isdigit()):
+            digit_count += 1
+        if(digit_count > 1 and c.isalpha()):
+            return False
+
+    # No periods, spaces, or punctuation marks are allowed
+    for c in s:
+        if c in [' ', '.', '!', '?']:
+            return False
+    return True
 
 
 if __name__ == "__main__":
