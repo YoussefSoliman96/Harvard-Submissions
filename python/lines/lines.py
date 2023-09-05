dc35809e09c9830f1c6cc3a2774165e84376187d
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-05 14:52:12 +0000

    Tue, Sep 5, 2023, 4:52 PM +02:00

--- python/lines/lines.py
+++ python/lines/lines.py
@@ -1,19 +1,26 @@
 import sys
 def main():
     argument_check()
-
+    # Open the file in command-line
     try:
         file = open(sys.argv[1], "r")
         lines = file.readlines()
         print(lines)
     except FileNotFoundError:
         sys.exit("File does not exit")
+    # Loop through each line in the file
+    for line in lines :
+         
+
+
 
 def argument_check():
+        # Check if command-line arguments are not 2
         if len(sys.argv) < 2:
             sys.exit("Too few command-line arguments")
         elif len(sys.argv) > 2:
             sys.exit("Too many command-line arguments")
+        # Check if file does not end with .py
         elif ".py" not in sys.argv[1]:
             sys.exit("Not a python file")
 
