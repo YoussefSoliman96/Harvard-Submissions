4dff1ae2dbc80d29a2889f373bd86267158ed3a7
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-05 14:49:32 +0000

    Tue, Sep 5, 2023, 4:49 PM +02:00

--- python/lines/lines.py
+++ python/lines/lines.py
@@ -10,15 +10,14 @@ def main():
         sys.exit("File does not exit")
 
 def argument_check():
-for arg in sys.argv:
-    if len(sys.argv) < 2:
-        sys.exit("Too few command-line arguments")
-    elif len(sys.argv) > 2:
-        sys.exit("Too many command-line arguments")
-    elif ".py" not in sys.argv[1]:
-        sys.exit("Not a python file")
+        if len(sys.argv) < 2:
+            sys.exit("Too few command-line arguments")
+        elif len(sys.argv) > 2:
+            sys.exit("Too many command-line arguments")
+        elif ".py" not in sys.argv[1]:
+            sys.exit("Not a python file")
 
 
 
-if __name__ == "__main__"
+if __name__ == "__main__":
     main()
