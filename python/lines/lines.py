5f21070f3c9a650db9d6ed3c3ddf1591c774c42e
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-05 13:48:46 +0000

    Tue, Sep 5, 2023, 3:48 PM +02:00

--- python/lines/lines.py
+++ python/lines/lines.py
@@ -1,9 +1,9 @@
 import sys
 
 for arg in sys.argv:
-    if str.len(sys.argv) < 2:
+    if len(sys.argv) < 2:
         print("Too few command-line arguments")
-    elif str.len(sys.argv > 2):
+    elif len(sys.argv) > 2:
         print("Too many command-line arguments")
     else:
         break
