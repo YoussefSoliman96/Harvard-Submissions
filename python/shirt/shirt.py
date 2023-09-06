b4cff6d1394813bd2e72a1543dfa111e95f1f7ae
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-06 08:30:29 +0000

    Wed, Sep 6, 2023, 10:30 AM +02:00

--- python/shirt/shirt.py
+++ python/shirt/shirt.py
@@ -7,17 +7,18 @@ def main():
 
     except FileNotFoundError:
         sys.exit("File does not Exist")
- 
+
 
 
 
 def argument_check():
+    extensions = [".jpg", ".jpeg", "png"]
     # Check if the command line arguments are 2
     if (len(sys.argv) < 3):
         sys.exit("Too few command-line arguments")
     elif (len(sys.argv) > 3):
         sys.exit("Too many command-line arguments")
-    elif ".jpg" or "jpeg" or ".png" not in sys.argv[1]:
+    elif ".jpg" or "jpeg" or ".png" not sys.argv[1]:
         sys.exit("Not a CSV file")
 
 
