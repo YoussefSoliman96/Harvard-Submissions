26a6038818549f9362ec7e8387c7bf1ad1b27539
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-06 08:27:36 +0000

    Wed, Sep 6, 2023, 10:27 AM +02:00

--- python/shirt/shirt.py
+++ python/shirt/shirt.py
@@ -3,18 +3,11 @@ def main():
     argument_check()
     output = []
     try:
-        with open(sys.argv[1], "r") as file:
-            reader = csv.DictReader(file)
-            for line in reader:
-                last_name, first_name = line["name"].split(",")
-                output.append()
+
 
     except FileNotFoundError:
         sys.exit("File does not Exist")
-    with open(sys.argv[2], "w") as file:
-            writer = csv.DictWriter(file, fieldnames=[])
-            writer.writerow({)
-            for line in output:
+ 
 
 
 
@@ -24,7 +17,7 @@ def argument_check():
         sys.exit("Too few command-line arguments")
     elif (len(sys.argv) > 3):
         sys.exit("Too many command-line arguments")
-    elif ".csv" not in sys.argv[1]:
+    elif ".jpg" or "jpeg" or ".png" not in sys.argv[1]:
         sys.exit("Not a CSV file")
 
 
