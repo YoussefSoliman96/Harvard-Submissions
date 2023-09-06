b24626d52f62307de87947f002e4b60188414545
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-06 13:20:01 +0000

    Wed, Sep 6, 2023, 3:20 PM +02:00

--- python/shirt/shirt.py
+++ python/shirt/shirt.py
@@ -1,10 +1,19 @@
 import sys
+from os.path import splitext
+from PIL import Image, ImageOps
 def main():
     argument_check()
-    file_type_check(file)
-    output = []
 
 
+
+
+
+# Check if input and output files are in correct format
+def file_type_check(file):
+     extensions = [".jpg", ".jpeg", "png"]
+     if file in extensions:
+         return True
+     return False
 
 # Check if the command line arguments are 2
 def argument_check():
@@ -15,20 +24,13 @@ def argument_check():
     # If command-line arguments pass the test, they are split into file_name and extensions
     first_name, first_extension = splitext(sys.argv[1])
     second_name, second_extension = splitext(sys.argv[2])
-
-
-# Check if input and output files are in correct format
-def file_type_check(file):
-     extensions = [".jpg", ".jpeg", "png"]
-     if file in extensions:
-         return True
-     return False
-
-
-
-
-
-
+    if first_extension == second_extension:
+        if file_type_check(first_extension) == False:
+            print("First file extension invalid")
+        elif file_type_check(second_extension) == False:
+            print("Second file extension invalid")
+    else:
+        print("Input and Output file got different formats")
 
 
 
