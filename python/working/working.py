b8a1c516f7b509ce52e4b5bc04908bef711e8f3f
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-07 10:32:42 +0000

    Thu, Sep 7, 2023, 12:32 PM +02:00

--- python/working/working.py
+++ python/working/working.py
@@ -7,7 +7,7 @@ def main():
 
 
 def convert(s):
-    format =  re.search(r"^([0-9]+):*([0-5]*) ([A-P]M) to ([0-9]+):*([0-5]*) ([A-P]M)$", s)
+    format =  re.search(r"^([0-9]+):*([0-5][0-9]*) ([A-P]M) to ([0-9]+):*([0-5][0-9]*) ([A-P]M)$", s)
     if format:
         groups = format.groups()
         if int(groups[0]) > 12 or int(groups[4]) > 12:
