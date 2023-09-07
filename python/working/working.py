8b47bc266bcf27679cf174a0ea62eaad0d10831f
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-07 08:31:33 +0000

    Thu, Sep 7, 2023, 10:31 AM +02:00

--- python/working/working.py
+++ python/working/working.py
@@ -7,7 +7,7 @@ def main():
 
 
 def convert(s):
-    if re.search("[0-9]+:*[0-5]*[0-9]* AM to [0-9]:*[0-5]*[0-9]* PM", s):
+    if re.search(r"^[0-9]+:*[0-5]*[0-9]* [A-P]M to [0-9]:*[0-5]*[0-9]* [A-P]M$", s):
         print(s)
 
 
