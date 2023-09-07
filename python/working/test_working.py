b11451e3264e3a3c949102d6d9e8a853bde1c20c
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-07 10:19:29 +0000

    Thu, Sep 7, 2023, 12:19 PM +02:00

--- python/working/test_working.py
+++ python/working/test_working.py
@@ -2,19 +2,30 @@ from working import convert
 import pytest
 
 def main():
-test_format()
+    test_format()
+    test_hour()
+    test_min()
 
 
 
 def test_format():
-    assert 
+    with pytest.raises(ValueError):
+        convert("5 AM 9 PM")
+        convert("5 AM : 9 PM")
+        convert("5 AM - 9 PM")
+
+def test_hour():
+    with pytest.raises(ValueError):
+        convert("5 AM to 13 PM")
+        convert("13 PM to 5 AM")
+
+def test_min():
+    with pytest.raises(ValueError):
+        convert("5:70 AM to 9:00 PM")
+        convert("5:00 AM to 9:65 PM")
 
 
 
 
-
-
-
-
-if __name__ = "__main__"
+if __name__ == "__main__":
     main()
