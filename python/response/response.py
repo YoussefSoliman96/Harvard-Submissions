2d6ba557d0e2cab0edf7a0362487fc335a82e8ae
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-08 15:13:42 +0000

    Fri, Sep 8, 2023, 5:13 PM +02:00

--- python/response/response.py
+++ python/response/response.py
@@ -2,20 +2,19 @@ import sys
 import re
 
 def main():
+    print(validator(input("What's your email address? ")))
 
 
 
 
 
 
+def validator():
 
 
 
 
 
 
-
-
-
-if __name__ == "__main__"
+if __name__ == "__main__":
     main()
