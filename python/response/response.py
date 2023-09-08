d038e736587f249897fe8fe37934011f55b071fc
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-08 16:12:46 +0000

    Fri, Sep 8, 2023, 6:12 PM +02:00

--- python/response/response.py
+++ python/response/response.py
@@ -4,13 +4,16 @@ import validators
 
 def main():
     email = input("What's your email address? ")
-    print(validators.email(email))
+    print(validation(email))
 
 
 
 def validation(email):
     validator = validators.email(email)
-    if validator == True
+    if validator == True:
+        return "Valid"
+    else:
+        return "Invalid"
 
 
 
