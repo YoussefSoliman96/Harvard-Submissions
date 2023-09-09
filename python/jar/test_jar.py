2cf3cd4fc6f577b8347a6c2b2eefe42fd50c2b54
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-09 16:10:42 +0000

    Sat, Sep 9, 2023, 6:10 PM +02:00

--- python/jar/test_jar.py
+++ python/jar/test_jar.py
@@ -1,1 +1,25 @@
 from jar import Jar
+
+def test_init():
+    jar = Jar()
+    assert jar.capacity == 12
+
+
+def test_str():
+    jar = Jar()
+    jar.deposit(1)
+    assert str(jar) == "🍪"
+
+def test_deposit():
+    jar = Jar()
+    jar.deposit(2)
+    assert jar.size == 2
+    jar.deposit(3)
+    assert jar.size == 5
+
+def test_deposit():
+    jar = Jar()
+    jar.deposit(5)
+    assert jar.size == 5
+    jar.withdraw(3)
+    assert jar.size == 2
