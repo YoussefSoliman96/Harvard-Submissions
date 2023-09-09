c8fe26831a7471070bdf7a1396782e1254eb1736
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-09 15:38:09 +0000

    Sat, Sep 9, 2023, 5:38 PM +02:00

--- python/jar/jar.py
+++ python/jar/jar.py
@@ -1,14 +1,19 @@
 class Jar:
     def __init__(self, capacity=12):
         self.capacity = capacity
+        self.size = 0
         if capacity < 0:
             raise ValueError("Invalid Input")
 
     def __str__(self):
-        ...
+        amount = self.size * "🍪"
+        return amount
 
     def deposit(self, n):
-        ...
+        if n < capacity:
+            self.size -= n
+        else:
+            raise ValueError("Amount requested is unavailable")
 
     def withdraw(self, n):
         ...
