2899ea6337044359161e529d73fc3c9d9b02be7e
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-09 17:33:28 +0000

    Sat, Sep 9, 2023, 7:33 PM +02:00

--- python/shirtificate/shirtificate.py
+++ python/shirtificate/shirtificate.py
@@ -7,11 +7,11 @@ def main():
             # Rendering logo:
             self.image("shirtificate.png", 10, 8, 33)
             # Setting font: helvetica bold 15
-            self.set_font("helvetica", "B", 15)
+            self.set_font("helvetica", "B", 40)
             # Moving cursor to the right:
             self.cell(80)
             # Printing title:
-            self.cell(30, 10, "Title", border=1, align="C")
+            self.cell(30, 10, "CS50 Shirtificate", align="C")
             # Performing a line break:
             self.ln(20)
 
@@ -19,7 +19,7 @@ def main():
             # Position cursor at 1.5 cm from bottom:
             self.set_y(-15)
             # Setting font: helvetica italic 8
-            self.set_font("helvetica", "I", 8)
+            self.set_font("helvetica", "I", 25)
             # Printing page number:
             self.cell(0, 10, f"Page {self.page_no()}/{{nb}}", align="C")
 
