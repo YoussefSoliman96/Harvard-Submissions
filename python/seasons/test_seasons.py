e8af7a1804173f28fa418fd573a55c67264abe04
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-09 09:02:20 +0000

    Sat, Sep 9, 2023, 11:02 AM +02:00

--- python/seasons/test_seasons.py
+++ python/seasons/test_seasons.py
@@ -1,1 +1,18 @@
 from seasons import get_date
+
+def main():
+    test_get_date()
+
+def test_get_date():
+    assert get_date("1996-07-24") == "Fourteen million, two hundred sixty-seven thousand, five hundred twenty minutes"
+    assert get_date("1996-7-24") == "Invalid format"
+    assert get_date("1996-07-2") == "Invalid format"
+    assert get_date("24-07-1996") == "Invalid format"
+    assert get_date("07-24-1996") == "Invalid format"
+
+
+
+
+
+if __name__ == "__main__":
+    main()
