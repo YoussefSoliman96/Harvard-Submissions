c4ee78551b7436a5de791919d06ba5e560a5f09f
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-09 17:56:53 +0000

    Sat, Sep 9, 2023, 7:56 PM +02:00

--- python/shirtificate/shirtificate.py
+++ python/shirtificate/shirtificate.py
@@ -3,7 +3,10 @@ from fpdf import FPDF
 def main():
 
     class PDF(FPDF):
-        def header(self):
+        def __init__(self, name):
+            pdf = PDF()
+            pdf.cell(20, 40, f"{name}", new_x="LMARGIN", new_y="NEXT")
+            pdf.output("new_shirtificate.pdf")
             # Rendering logo:
             self.image("shirtificate.png", 10, 50, w=pdf.epw)
             # Setting font: helvetica bold 15
@@ -12,17 +15,11 @@ def main():
             self.cell(0, 10, "CS50 Shirtificate", align="C")
             # Performing a line break:
             self.ln(20)
+            pdf.add_page()
+            pdf.set_font("Times", "B", 40)
+            pdf.set_text_color(255, 255, 255)
 
-    # Instantiation of inherited class
-    pdf = PDF()
-    pdf.add_page()
-    pdf.set_font("Times", "B", 40)
-    pdf.set_text_color(255, 255, 255)
     name = input("Name: ")
-    pdf.cell(20, 40, f"{name}", new_x="LMARGIN", new_y="NEXT")
-    pdf.output("new_shirtificate.pdf")
-
-
 
 
 if __name__ == "__main__":
