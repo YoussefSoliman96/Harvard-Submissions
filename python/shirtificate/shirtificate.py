1d0ff473518795471d8a95521fa1807468a06b83
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-09 16:39:02 +0000

    Sat, Sep 9, 2023, 6:39 PM +02:00

--- python/shirtificate/shirtificate.py
+++ python/shirtificate/shirtificate.py
@@ -2,8 +2,8 @@ from fpdf import FPDF
 pdf = FPDF()
 pdf.add_page()
 pdf.set_font('helvetica', size=12)
-pdf.cell(txt="hello world")
-pdf.output("hello_world.pdf")
+pdf.cell(txt="took CS50")
+pdf.output("shirtificate.pdf")
 
 def main():
 
