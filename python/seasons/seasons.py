9d4fd855f364da4eebc8ce33124bef8d2e382cdb
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-09 07:56:39 +0000

    Sat, Sep 9, 2023, 9:56 AM +02:00

--- python/seasons/seasons.py
+++ python/seasons/seasons.py
@@ -2,20 +2,20 @@ from datetime import date
 import re
 import sys
 
-
 def main():
     date = input("Date of Birth: ")
-    new_date = get_date(date)
-    print(new_date)
+    try:
+         year, month, day = get_date(date)
+    except:
+        sys.exit("Invalid_format")
+    print(year, month, day)
 
 def get_date(date):
-    try:
-        if re.search(r"(^[0-9]{4})-([0-9]{2})-([0-9]{2})$", date):
-            year, month, day = date.split("-")
-            return(year, month, day)
-    except:
-        sys.exit("Invalid date")
-        
+
+    if re.search(r"(^[0-9]{4})-([0-9]{2})-([0-9]{2})$", date):
+        year, month, day = date.split("-")
+        return(year, month, day)
+
 
 if __name__ == "__main__":
     main()
