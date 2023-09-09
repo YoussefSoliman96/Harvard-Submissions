91054f804b79f49f54eb984925fb1c32a9832af1
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-09 08:44:59 +0000

    Sat, Sep 9, 2023, 10:44 AM +02:00

--- python/seasons/seasons.py
+++ python/seasons/seasons.py
@@ -5,13 +5,17 @@ import sys
 def main():
     date_of_birth = input("Date of Birth: ")
     try:
+         # Exit the program if the user inputs a wrong format
          year, month, day = get_date(date_of_birth)
     except:
         sys.exit("Invalid format")
-    print(date(int(year), int(month), int(day)))
+    # Get the original date of birth and today's date to calculate the number of days between them
+    original_date = date(int(year), int(month), int(day))
+    today_date = original_date.today()
+
 
 def get_date(date):
-
+    # Make sure the entered date is in correct format
     if re.search(r"(^[0-9]{4})-([0-9]{2})-([0-9]{2})$", date):
         year, month, day = date.split("-")
         return(year, month, day)
