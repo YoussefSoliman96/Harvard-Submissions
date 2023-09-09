fbb58d8072b65ea15632d29bfb4e2c6340fe7cc0
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-09 17:47:12 +0000

    Sat, Sep 9, 2023, 7:47 PM +02:00

--- python/shirtificate/shirtificate.py
+++ python/shirtificate/shirtificate.py
@@ -5,7 +5,7 @@ def main():
     class PDF(FPDF):
         def header(self):
             # Rendering logo:
-            self.image("shirtificate.png", 0, 0, 0)
+            self.image("shirtificate.png", 0, 0, w=pdf.epw)
             # Setting font: helvetica bold 15
             self.set_font("helvetica", "B", 40)
             # Printing title:
@@ -13,13 +13,7 @@ def main():
             # Performing a line break:
             self.ln(20)
 
-        def footer(self):
-            # Position cursor at 1.5 cm from bottom:
-            self.set_y(-15)
-            # Setting font: helvetica italic 8
-            self.set_font("helvetica", "I", 25)
-            # Printing page number:
-            self.cell(0, 10, f"Page {self.page_no()}/{{nb}}", align="C")
+
 
 
     # Instantiation of inherited class
