a2df99bee739f981ba4364f68d45a25fe4bb9409
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-09 16:37:26 +0000

    Sat, Sep 9, 2023, 6:37 PM +02:00

---
+++ python/shirtificate/shirtificate.py
@@ -0,0 +1,26 @@
+from fpdf import FPDF
+pdf = FPDF()
+pdf.add_page()
+pdf.set_font('helvetica', size=12)
+pdf.cell(txt="hello world")
+pdf.output("hello_world.pdf")
+
+def main():
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ = "__main__"
+    main()
