34a39fa7a4bc622cb190307ad2e159c85d64873d
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-09 15:50:43 +0000

    Sat, Sep 9, 2023, 5:50 PM +02:00

--- python/jar/jar.py
+++ python/jar/jar.py
@@ -1,7 +1,7 @@
 class Jar:
     def __init__(self, capacity=12):
-        self.capacity = capacity
-        self.size = 0
+        self._capacity = capacity
+        self._size = 0
         if capacity < 0:
             raise ValueError("Invalid Input")
 
@@ -15,24 +15,24 @@ class Jar:
         elif (n + self.size) > self.capacity:
             raise ValueError("Amount requested is too big")
         else:
-            self.size += n
+            self._size += n
 
     def withdraw(self, n):
         if n > self.size:
             raise ValueError("Amount requested is unavailable")
         else:
-            self.size -= n
+            self._size -= n
 
 
     @property
     def capacity(self):
-        return self.capacity
-"""
+        return self._capacity
+
 
     @property
     def size(self):
-        ...
-"""
+        return self._size
+
 
 jar = Jar()
 jar.deposit(5)
