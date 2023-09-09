272b025f620c3d4f773b0a074d28a922e68fcf6a
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-09 15:31:11 +0000

    Sat, Sep 9, 2023, 5:31 PM +02:00

--- python/jar/jar.py
+++ python/jar/jar.py
@@ -1,6 +1,8 @@
 class Jar:
     def __init__(self, capacity=12):
         self.capacity = capacity
+        if capacity < 0:
+            raise ValueError("Invalid Input")
 
     def __str__(self):
         ...
