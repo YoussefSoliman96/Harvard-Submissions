aeab3cd7012c014512ab0d6db93f41c0ef96276d
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-10 07:50:30 +0000

    Sun, Sep 10, 2023, 9:50 AM +02:00

--- python/Final/project.py
+++ python/Final/project.py
@@ -1,12 +1,12 @@
 
 
-class Client:
-    def __init__(self, first_name, last_name, email, savings)
-        self.first_name = first_name
-        self.last_name = last_name
-        self.email = email
 
 def main():
+    class Client:
+        def __init__(self, first_name, last_name, email, savings):
+            self.first_name = first_name
+            self.last_name = last_name
+            self.email = email
 
 
 
