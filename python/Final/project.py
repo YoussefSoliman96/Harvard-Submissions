ca990d941645a07a04116a4b94c690278c361e87
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-10 19:33:33 +0000

    Sun, Sep 10, 2023, 9:33 PM +02:00

--- python/Final/project.py
+++ python/Final/project.py
@@ -1,7 +1,7 @@
 import csv
 import sys
 
-"""
+
 def main():
     class Client:
         def __init__(self, first_name, last_name, email, savings):
@@ -9,6 +9,38 @@ def main():
             self.last_name = last_name
             self.email = email
 
+# Read the file containing clients' data
+def read_file():
+    clients = []
+    try:
+        # Open the file containing clients' data
+        with open("clients.csv", "r") as csv_file:
+            csv_reader = csv.DictReader(csv_file)
+            # Loop through the file and append data to the clients stack
+            for line in csv_reader:
+                clients.append({'first_name': line["first_name"], 'last_name': line["last_name"], 'email': line["email"], 'savings': line["savings"]})
+        return clients
+    except FileNotFoundError:
+        sys.exit("File not found")
+
+# Add new clients to the Clients' file
+def write_file():
+    with open(sys.argv[2], "w") as file:
+        writer = csv.DictWriter(file, fieldnames=["first", "last", "house"])
+        writer.writerow({"first": "first", "last": "last", "house": "house"})
+        for line in output:
+            writer.writerow({"first": line["first"], "last": line["last"], "house": line["house"]})
+
+
+
+
+if __name__ == "__main__"
+    main()
+
+
+
+
+"""
 
 
 def get_date():
@@ -25,28 +57,4 @@ def withdraw():
 
 def calculate_savings():
 
-
-
-
-
-
-if __name__ == "__main__"
-    main()
-
 """
-def read_file():
-    clients = []
-    try:
-        # Open the file containing clients' data
-        with open("clients.csv", "r") as csv_file:
-            csv_reader = csv.DictReader(csv_file)
-            # Loop through the file and append data to the clients stack
-            for line in csv_reader:
-                clients.append({'first_name': line["first_name"], 'last_name': line["last_name"], 'email': line["email"], 'savings': line["savings"]})
-        return clients
-    except FileNotFoundError:
-        sys.exit("File not found")
-
-client = read_file()
-print(client)
-
