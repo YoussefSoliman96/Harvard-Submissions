f0d6179fbd007b15be7cd0eef119f9b59b1ced3b
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-11 21:49:00 +0000

    Mon, Sep 11, 2023, 11:49 PM +02:00

--- python/Final/test_project.py
+++ python/Final/test_project.py
@@ -1,9 +1,18 @@
 from project import get_data
+import pytest
 
-test_get_data()
+
+def main():
+    test_get_data()
 
 
 
 
 def test_get_data():
-    assert get_data(client_data) == 
+    assert get_data("Harry Potter") == "{'id': '0', 'first_name': 'Harry', 'last_name': 'Potter', 'email': 'harry.potter@hogwarts.com', 'balance': '9500'}"
+
+
+
+
+if __name__ == "__main__":
+    main()
