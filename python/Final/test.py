156bef576248249a1b47d59261a0e0faa0f1d156
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-11 09:13:22 +0000

    Mon, Sep 11, 2023, 11:13 AM +02:00

--- python/Final/test.py
+++ python/Final/test.py
@@ -1,1 +1,9 @@
 from datetime import datetime
+from enum import StrEnum, auto
+
+class TransactionType(enum):
+    Deposit = auto()
+    Withdraw = auto()
+
+
+Transaction = tuple [TransactionType, datetime, int]
