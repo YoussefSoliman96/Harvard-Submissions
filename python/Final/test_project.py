f670944ad8332a1c78046c04e6d5771802a5ac9e
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-11 22:49:56 +0000

    Tue, Sep 12, 2023, 12:49 AM +02:00

--- python/Final/test_project.py
+++ python/Final/test_project.py
@@ -1,21 +1,32 @@
-from project import options
-from projecy import get_data
-import pytest
-
+from project import Client
 
 def main():
-    test_options()
-    test_get_data()
+def test_init():
+    client = Client()
+    assert client.balance == 500
+    new_client = Client(200)
+    assert new_client.balance == 200
 
 
-def test_options():
-    choices = ["Deposit", "Withdraw", "Print statement"]
-    i = 1
-    assert options() == "Deposit"
+def test_str():
+    jar = Jar()
+    assert str(jar) == ""
+    jar.deposit(1)
+    assert str(jar) == "🍪"
 
-def test_get_data()
-    assert
+def test_deposit():
+    jar = Jar()
+    jar.deposit(2)
+    assert jar.size == 2
+    jar.deposit(3)
+    assert jar.size == 5
 
+def test_withdraw():
+    jar = Jar()
+    jar.deposit(5)
+    assert jar.size == 5
+    jar.withdraw(3)
+    assert jar.size == 2
 
 
 
