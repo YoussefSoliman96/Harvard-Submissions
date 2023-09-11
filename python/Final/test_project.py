5ce8572806626723d625559c2d45c5549cd3f0fd
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-11 22:27:37 +0000

    Tue, Sep 12, 2023, 12:27 AM +02:00

--- python/Final/test_project.py
+++ python/Final/test_project.py
@@ -1,14 +1,18 @@
 from project import options
+from projecy import get_data
 import pytest
 
 
 def main():
     test_options()
+    test_get_data()
 
 
 def test_options():
     assert options() == "Deposit"
 
+def test_get_data()
+    assert
 
 
 
