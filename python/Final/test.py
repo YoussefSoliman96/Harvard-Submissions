664f6fc503466878704cd03e8aff7cb1433662a6
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-11 17:16:37 +0000

    Mon, Sep 11, 2023, 7:16 PM +02:00

--- python/Final/test.py
+++ python/Final/test.py
@@ -1,7 +1,10 @@
 from datetime import datetime
 import sys
 def main():
-
+    
+    now = datetime.now()
+    dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+    print("date and time =", dt_string)
 
 if __name__ == "__main__":
     main()
