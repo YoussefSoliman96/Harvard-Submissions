55b54b1a2c5194dfc35340ae77d37b3a2eb55f3e
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-11 23:35:22 +0000

    Tue, Sep 12, 2023, 1:35 AM +02:00

--- python/Final/test_project.py
+++ python/Final/test_project.py
@@ -1,18 +1,25 @@
 from unittest.mock import Mock
-from project import options
+from project import operation
 from project import get_data
 import pytest
 
 
 def main():
-    test_options()
+    test_operation()
 
 
 
-def test_options():
-    Options = options()
-    Options.get = Mock(return_value = 1)
-    assert options() == "Deposit"
+def test_operation():
+    def side_effect(arg):
+        return values[arg]
+
+    mock.side_effect = side_effect
+    mock('a'), mock('b'), mock('c')
+    (1, 2, 3)
+    mock.side_effect = [5, 4, 3, 2, 1]
+    mock(), mock(), mock()
+    (5, 4, 3)
+
 """
 def test_get_data()
     assert
