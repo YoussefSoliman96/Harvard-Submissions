edc6cb944c05b9b86bf8b45821e29696946db8a6
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-11 15:11:22 +0000

    Mon, Sep 11, 2023, 5:11 PM +02:00

--- python/Final/test.py
+++ python/Final/test.py
@@ -1,14 +1,19 @@
 from datetime import datetime
+def main():
+    choices = ["Deposit", "Withdraw", "Print statement"]
+    options(choices)
 
+    def options(choices):
+        print("Please choose:")
+        for idx, element in enumerate(choices):
+            print("{}) {}".format(idx+1,element))
+        i = input("Enter number: ")
+        try:
+            if 0 < int(i) <= len(choices):
+                return int(i)
+        except:
+            pass
+        return None
 
-def options(choices):
-    print("Please choose:")
-    for idx, element in enumerate(choices):
-        print("{}) {}".format(idx+1,element))
-    i = input("Enter number: ")
-    try:
-        if 0 < int(i) <= len(options):
-            return int(i)
-    except:
-        pass
-    return None
+if __name__ == "__main__":
+    main()
