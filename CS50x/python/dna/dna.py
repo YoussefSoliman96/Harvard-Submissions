d95038a44b55df243e810f48143fa78866de80b3
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-13 21:03:07 +0000

    Wed, Sep 13, 2023, 11:03 PM +02:00

--- CS50x/python/dna/dna.py
+++ CS50x/python/dna/dna.py
@@ -22,7 +22,7 @@ def main():
     # TODO: Read database file into a variable
     database = read_database(1)
     print(database)
-    print(database[0])
+    print(database['name'])
     # TODO: Read DNA sequence file into a variable
     dna_sequence = read_sequence(2)
     # TODO: Find longest match of each STR in DNA sequence
