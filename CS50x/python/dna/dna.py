986bd53c897c798393c6354a074d10c4b9049384
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-13 21:38:27 +0000

    Wed, Sep 13, 2023, 11:38 PM +02:00

--- CS50x/python/dna/dna.py
+++ CS50x/python/dna/dna.py
@@ -39,7 +39,7 @@ def main():
             if match == len(subsequences):
                 print(name["name"])
                 return
-    print("Not Found")
+    print("No match")
 
 def read_database(n):
     database = []
