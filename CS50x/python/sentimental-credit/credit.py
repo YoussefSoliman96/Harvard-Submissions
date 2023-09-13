24ec07e1d04b94255b51cc0f8f27238adddee26d
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-13 15:40:55 +0000

    Wed, Sep 13, 2023, 5:40 PM +02:00

--- CS50x/python/sentimental-credit/credit.py
+++ CS50x/python/sentimental-credit/credit.py
@@ -23,8 +23,8 @@ def validate(number):
     print(even_summed)
 
     odd_positions = ([number[i] for i in filter(lambda a: a % 2 == 1, range(len(number)))])
-
-    print(multiplied)
+    odd_positions = [int(i) for i in odd_positions]
+    print(odd_positions)
 
 
 
