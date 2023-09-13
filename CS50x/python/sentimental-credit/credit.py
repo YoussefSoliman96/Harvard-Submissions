595f15bea5d17298fe17549939d3c614d0fff399
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-13 15:35:56 +0000

    Wed, Sep 13, 2023, 5:35 PM +02:00

--- CS50x/python/sentimental-credit/credit.py
+++ CS50x/python/sentimental-credit/credit.py
@@ -13,17 +13,27 @@ def main():
 
 def validate(number):
     multiplied = []
+    even_numbers = ([number[i] for i in filter(lambda a: a % 2 == 0, range(len(number)))]) [::-1]
+
+    for j in range(len(even_numbers)):
+        multiplied.append(int((even_numbers[j])) * 2)
+
+
+    even_summed = sum(multiplied)
+    print(even_summed)
+
+    odd_numbers = ([number[i] for i in filter(lambda a: a % 1 == 0, range(len(number)))])
+
+    odd_summed = sum(int(odd_numbers))
+    print(odd_summed)
+
+
+
+def sum(number):
     summed = 0
-    every_other_number_reversed = ([number[i] for i in filter(lambda a: a % 2 == 0, range(len(number)))]) [::-1]
-
-    for j in range(len(every_other_number_reversed)):
-        multiplied.append(int((every_other_number_reversed[j])) * 2)
-
-
-    for k in range(len(multiplied)):
-        summed += multiplied[int(k)]
-
-
+    for k in range(len(number)):
+        summed += number[int(k)]
+    return summed
 
 if __name__ == "__main__":
     main()
