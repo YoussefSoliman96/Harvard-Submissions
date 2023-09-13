f2f89c54c2e48704c7ffdcbdf6d73be16984dedc
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-13 17:48:07 +0000

    Wed, Sep 13, 2023, 7:48 PM +02:00

--- CS50x/python/sentimental-credit/credit.py
+++ CS50x/python/sentimental-credit/credit.py
@@ -23,51 +23,50 @@ def main():
         except ValueError:
             print("Invalid input")
 
+    def validate(number):
+        multiplied = []
+        reversed = number[::-1]
+        # Identify every other number starting from index 0 then reversing them
+        even_positions = [
+            reversed[i] for i in filter(lambda a: a % 2 != 0, range(len(reversed)))
+        ]
+        # multiply every other number by 2
+        for j in range(len(even_positions)):
+            multiplied.append(int((even_positions[j])) * 2)
 
-def validate(number):
-    multiplied = []
-    reversed = number[::-1]
-    # Identify every other number starting from index 0 then reversing them
-    even_positions = [
-        reversed[i] for i in filter(lambda a: a % 2 != 0, range(len(reversed)))
-    ]
-    # multiply every other number by 2
-    for j in range(len(even_positions)):
-        multiplied.append(int((even_positions[j])) * 2)
+        # Modified multiplied with single digits only
+        modified = []
 
-    # Modified multiplied with single digits only
-    modified = []
+        for i in range(len(multiplied)):
+            if len(str(multiplied[i])) > 1:
+                split = divmod(multiplied[i], 10)
+                modified.append(split[0])
+                modified.append(split[1])
+            else:
+                modified.append(multiplied[i])
 
-    for i in range(len(multiplied)):
-        if len(str(multiplied[i])) > 1:
-            split = divmod(multiplied[i], 10)
-            modified.append(split[0])
-            modified.append(split[1])
+        # Adding the numbers in the multiplied list together
+        even_summed = sum(modified)
+
+        # Identifying every other number that was not multiplied and adding them together
+        odd_positions = [
+            reversed[i] for i in filter(lambda a: a % 2 == 0, range(len(reversed)))
+        ]
+        odd_positions = [int(i) for i in odd_positions]
+        odd_summed = sum(odd_positions)
+
+        # Adding the 2 sums together
+        sum_all = even_summed + odd_summed
+
+        # Getting the last digit
+        sum_all = [int(i) for i in str(sum_all)]
+        last_digit = sum_all[-1]
+
+        # Return Valid if last digit is "0", otherwise, return Invalid
+        if last_digit == 0:
+            return "Valid"
         else:
-            modified.append(multiplied[i])
-
-    # Adding the numbers in the multiplied list together
-    even_summed = sum(modified)
-
-    # Identifying every other number that was not multiplied and adding them together
-    odd_positions = [
-        reversed[i] for i in filter(lambda a: a % 2 == 0, range(len(reversed)))
-    ]
-    odd_positions = [int(i) for i in odd_positions]
-    odd_summed = sum(odd_positions)
-
-    # Adding the 2 sums together
-    sum_all = even_summed + odd_summed
-
-    # Getting the last digit
-    sum_all = [int(i) for i in str(sum_all)]
-    last_digit = sum_all[-1]
-
-    # Return Valid if last digit is "0", otherwise, return Invalid
-    if last_digit == 0:
-        return "Valid"
-    else:
-        return "Invalid"
+            return "Invalid"
 
 
 def sum(number):
