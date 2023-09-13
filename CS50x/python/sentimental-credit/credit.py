31a0de80f94ccc3c881c67448dd9553dbcab37c6
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-13 15:47:11 +0000

    Wed, Sep 13, 2023, 5:47 PM +02:00

--- CS50x/python/sentimental-credit/credit.py
+++ CS50x/python/sentimental-credit/credit.py
@@ -13,18 +13,25 @@ def main():
 
 def validate(number):
     multiplied = []
+    # Identify every other number starting from index 0 then reversing them
     even_positions = ([number[i] for i in filter(lambda a: a % 2 == 0, range(len(number)))]) [::-1]
-
+    # multiply every other number by 2
     for j in range(len(even_positions)):
         multiplied.append(int((even_positions[j])) * 2)
 
-
+    # Adding the numbers in the multiplied list together
     even_summed = sum(multiplied)
     print(even_summed)
 
+    # Identifying every other number that was not multiplied and adding them together
     odd_positions = ([number[i] for i in filter(lambda a: a % 2 == 1, range(len(number)))])
     odd_positions = [int(i)*2 for i in odd_positions]
-    print(odd_positions)
+    odd_summed = sum(odd_positions)
+
+    # Adding the 2 sums together
+    sum_all = even_summed + odd_summed
+
+    print(sum_all)
 
 
 
