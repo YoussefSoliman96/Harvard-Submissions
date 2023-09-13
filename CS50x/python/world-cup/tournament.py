972cc11b0d313bb14b79182b26006457a043bf99
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-13 22:29:39 +0000

    Thu, Sep 14, 2023, 12:29 AM +02:00

--- CS50x/python/world-cup/tournament.py
+++ CS50x/python/world-cup/tournament.py
@@ -23,14 +23,13 @@ def main():
                 teams.append(line)
     except FileNotFoundError:
         print("File not found")
-    print(teams)
+    print(teams[0])
 
     counts = {}
     # TODO: Simulate N tournaments and keep track of win counts
-    teams
 
-    print(game)
-    return game
+
+    return
     # Print each team's chances of winning, according to simulation
     for team in sorted(counts, key=lambda team: counts[team], reverse=True):
         print(f"{team}: {counts[team] * 100 / N:.1f}% chance of winning")
