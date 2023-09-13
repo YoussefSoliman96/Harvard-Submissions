43f8ba6ffe16dfd5e01c4d149d455d05863c6b31
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-13 15:00:41 +0000

    Wed, Sep 13, 2023, 5:00 PM +02:00

--- CS50x/python/sentimental-credit/credit.py
+++ CS50x/python/sentimental-credit/credit.py
@@ -14,15 +14,16 @@ def main():
 def validate(number):
     every_other_number = []
     multiply = []
-    for i in range(len(number)):
-        if i % 2 == 0:
-            every_other_number.append(number[i])
-    every_other_number_reversed = every_other_number [::-1]
+    every_other_number = (number[i] for i in (lambda a: a % 2 == 0, range(len(number))))
+    print(every_other_number)
 
+"""
+    every_other_number_reversed = every_other_number [::-1]
     for j in range(len(every_other_number_reversed)):
         every_other_number_reversed[j]
         multiply.append(int((every_other_number_reversed[j])) * 2)
     print(multiply)
+    """
 
 
 
@@ -30,5 +31,5 @@ if __name__ == "__main__":
     main()
 
     """
-    one = (every_other_number[i] for i in (lambda a:a))
+    every_other_number = (number[i] for i in (lambda a: a % 2 == 0, range(len(number))))
     """
