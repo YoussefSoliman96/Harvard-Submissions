0c173c08816e6547c5a30f6bc071bb65675f6542
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-13 18:26:10 +0000

    Wed, Sep 13, 2023, 8:26 PM +02:00

--- CS50x/python/sentimental-readability/readability.py
+++ CS50x/python/sentimental-readability/readability.py
@@ -1,6 +1,11 @@
 def main():
 
     text = str(input("Text: "))
+    letters = get_letters()
+    words = get_words()
+    L = float(letters) / float(words) * 100
+    S = float(words) / float(sentences) * 100
+    index = 0.0588 * L - 0.296 * S - 15.8
 
 
 
