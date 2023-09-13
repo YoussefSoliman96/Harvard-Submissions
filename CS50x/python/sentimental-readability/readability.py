a44a22137810ab7645da9b7d33451ba0679f618f
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-13 18:54:55 +0000

    Wed, Sep 13, 2023, 8:54 PM +02:00

--- CS50x/python/sentimental-readability/readability.py
+++ CS50x/python/sentimental-readability/readability.py
@@ -1,13 +1,13 @@
 def main():
 
     text = str(input("Text: "))
-    letters = get_letters()
-    words = get_words()
-    sentences
+    letters = get_letters(text)
+    words = get_words(text)
+    sentences = get_sentences(text)
     L = float(letters) / float(words) * 100
     S = float(words) / float(sentences) * 100
     index = 0.0588 * L - 0.296 * S - 15.8
-
+    get_grade(index)
 
 
 
@@ -15,10 +15,9 @@ def main():
 def get_letters(text):
     count = 0
     for c in text:
-        if c == " ":
+        if c.isalpha():
             count += 1
     return count
-
 
 def get_words(text):
     count = 0
@@ -27,8 +26,20 @@ def get_words(text):
             count += 1
     return count
 
+def get_sentences(text):
+    count = 0
+    for c in text:
+        if c == "," or c == "." or c == "?":
+            count += 1
+    return count
 
-
+def get_grade(index):
+    if index < 1:
+        print("Before Grade 1")
+    elif index > 16:
+        print("Grade 16+")
+    else:
+        print(f"Grade: {index}")
 
 
 if __name__ == "__main__":
