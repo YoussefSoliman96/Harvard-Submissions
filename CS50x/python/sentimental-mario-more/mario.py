ef7d58b40076f8f074932864d70d057a53bf68a9
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-13 10:07:23 +0000

    Wed, Sep 13, 2023, 12:07 PM +02:00

--- CS50x/python/sentimental-mario-more/mario.py
+++ CS50x/python/sentimental-mario-more/mario.py
@@ -6,4 +6,7 @@ while True:
         print("Input is not an int")
 
 for i in range(height):
-    print(" " * int(height - i + 2) + "#" * int(i+1))
+    print(" " * int(height - i - 1) + "#" * int(i+1)"#" * int(i+1))
+
+for i in range(height):
+    print()
