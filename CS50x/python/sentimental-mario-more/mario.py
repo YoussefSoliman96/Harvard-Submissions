42522d0bb7b0ff460821a3a0d5421189d69f8ef7
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-13 10:12:10 +0000

    Wed, Sep 13, 2023, 12:12 PM +02:00

--- CS50x/python/sentimental-mario-more/mario.py
+++ CS50x/python/sentimental-mario-more/mario.py
@@ -1,11 +1,10 @@
 while True:
     try:
         height = int(input("Height: "))
-        if height > 0 and height <=8:
+        if height > 0 and height <= 8:
             break
     except ValueError:
         print("Input is not an int")
 
 for i in range(height):
-    print(" " * int(height - i - 1) + "#" * int(i+1) + "  " +  "#" * int(i+1))
-
+    print(" " * int(height - i - 1) + "#" * int(i + 1) + "  " + "#" * int(i + 1))
