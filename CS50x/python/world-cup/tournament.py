40c6f947d0e10e979ddbeb42b883755c72dcf582
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-13 22:18:53 +0000

    Thu, Sep 14, 2023, 12:18 AM +02:00

--- CS50x/python/world-cup/tournament.py
+++ CS50x/python/world-cup/tournament.py
@@ -27,7 +27,10 @@ def main():
 
     counts = {}
     # TODO: Simulate N tournaments and keep track of win counts
+    teams
 
+    print(game)
+    return game
     # Print each team's chances of winning, according to simulation
     for team in sorted(counts, key=lambda team: counts[team], reverse=True):
         print(f"{team}: {counts[team] * 100 / N:.1f}% chance of winning")
