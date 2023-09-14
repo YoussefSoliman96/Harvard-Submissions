3278fda5857799169e054c9aa93d833a91d54f68
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-14 23:35:56 +0000

    Fri, Sep 15, 2023, 1:35 AM +02:00

--- CS50x/SQL/movies/6.sql
+++ CS50x/SQL/movies/6.sql
@@ -1,2 +1,2 @@
 write a SQL query to determine the average rating of all movies released in 2012.
-SELECT AVG(rating) FROM movies JOIN ratings ON movie_id = (SELECT id FROM movies WHERE year = 2012);
+SELECT AVG(rating) FROM movies JOIN ratings ON movies.id = (SELECT id FROM movies WHERE year = 2012);
