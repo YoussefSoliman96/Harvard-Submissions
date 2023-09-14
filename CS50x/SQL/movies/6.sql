036a3181a94eaf52b7798305aff87fe547b8d92f
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-14 23:41:43 +0000

    Fri, Sep 15, 2023, 1:41 AM +02:00

--- CS50x/SQL/movies/6.sql
+++ CS50x/SQL/movies/6.sql
@@ -1,1 +1,1 @@
-SELECT AVG(rating) FROM movies JOIN ratings ON movies.id = (SELECT id FROM movies WHERE year = 2012);
+SELECT AVG(rating) FROM (movies JOIN ratings ON movies.id = ratings.movie_id) WHERE year = 2012;
