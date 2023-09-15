2271318086ce005852b01d40b13897d0ed4f4cbf
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-15 00:26:37 +0000

    Fri, Sep 15, 2023, 2:26 AM +02:00

--- CS50x/SQL/movies/10.sql
+++ CS50x/SQL/movies/10.sql
@@ -2,4 +2,4 @@ SELECT name FROM people
 JOIN directors ON directors.person_id = people.id
 JOIN movies ON movies.id = directors.movie_id
 JOIN ratings ON ratings.movie_id = movies.id
-WHERE rating >= 9
+WHERE ratings.rating >= 9
--- CS50x/SQL/movies/11.sql
+++ CS50x/SQL/movies/11.sql
@@ -3,6 +3,6 @@ write a SQL query to list the titles of the five highest rated movies (in order)
 SELECT tile FROM movies
 JOIN ratings ON ratings.movie_id = movies.id
 JOIN stars ON stars.movie_id = movies.id
-JOIN people ON people.id = stars.person_id
-WHERE name = "Chadwick Boseman"
+JOIN people ON stars.person_id = people.id
+WHERE people.name = "Chadwick Boseman"
 AND
