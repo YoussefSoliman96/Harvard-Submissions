973f7c69f47c563269513e69d4f2da73bebaeb5c
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-15 00:39:41 +0000

    Fri, Sep 15, 2023, 2:39 AM +02:00

--- CS50x/SQL/movies/13.sql
+++ CS50x/SQL/movies/13.sql
@@ -3,4 +3,8 @@ write a SQL query to list the names of all people who starred in a movie in whic
 SELECT name FROM people
 JOIN stars ON stars.movie_id = movies.id
 JOIN movies ON movies.id = stars.movie_id
-WHERE name = "
+WHERE movies.id in
+(SELECT id FROM movies
+JOIN stars ON stars.person_id = people.id
+JOIN people ON people.id = stars.movie_id
+WHERE people.name = "Kevin Bacon")
