ef9cb27bb14270f1c6f7fc3a1fb0b28bc945deb5
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-15 00:15:17 +0000

    Fri, Sep 15, 2023, 2:15 AM +02:00

--- CS50x/SQL/movies/10.sql
+++ CS50x/SQL/movies/10.sql
@@ -3,3 +3,5 @@ write a SQL query to list the names of all people who have directed a movie that
 SELECT name FROM people
 JOIN directors ON directors.person_id = people.id
 JOIN movies ON movies.id = directors.movie_id
+JOIN ratings ON ratings.movie_id = movies.id
+WHERE rating >= 9
