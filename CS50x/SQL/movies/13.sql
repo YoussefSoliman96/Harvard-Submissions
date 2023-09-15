de71390eefdaf4ea36e8ff857695df4a6f4648df
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-15 00:41:47 +0000

    Fri, Sep 15, 2023, 2:41 AM +02:00

--- CS50x/SQL/movies/13.sql
+++ CS50x/SQL/movies/13.sql
@@ -4,7 +4,7 @@ SELECT name FROM people
 JOIN stars ON stars.movie_id = movies.id
 JOIN movies ON movies.id = stars.movie_id
 WHERE movies.id in
-(SELECT id FROM movies
-JOIN stars ON stars.person_id = people.id
-JOIN people ON people.id = stars.movie_id
+(SELECT movies.id FROM movies
+JOIN stars ON stars.movie_id = movies.id
+JOIN people ON people.id = stars.person_id
 WHERE people.name = "Kevin Bacon")
