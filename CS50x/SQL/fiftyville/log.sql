fd7b904ad3dd8ce80da17829be6c0244c8ba9771
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-15 02:29:49 +0000

    Fri, Sep 15, 2023, 4:29 AM +02:00

--- CS50x/SQL/fiftyville/log.sql
+++ CS50x/SQL/fiftyville/log.sql
@@ -1,10 +1,9 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
-SELECT description FROM crime_scene_reports WHERE year = 2021 AND month = 7 AND day = 28 AND street = "Humphrey Street";
+SELECT description FROM crime_scene_reports WHERE year = 2021 AND month = 7 AND day = 28 AND street = "Blumberg Boulevard";
 SELECT transcript FROM interviews WHERE year = 2021 AND month = 7 AND day = 28;
 
-Theft of the CS50 duck took place at 10:15am at the Humphrey Street bakery. Interviews were conducted today with three witnesses who were present at the time –
-each of their interview transcripts mentions the bakery. |
-Littering took place at 16:36. No known witnesses.
+Theft of the CS50 duck took place at 10:15am at the Humphrey Street bakery. Interviews were conducted today with three witnesses who were present at the time
+ each of their interview transcripts mentions the bakery. Littering took place at 16:36. No known witnesses.
 
 SELECT name FROM people WHERE license_plate = (SELECT license_plate FROM bakery_security_logs WHERE year = 2021 AND month = 7 AND day = 28 AND hour = 10); --> Brandon
 
