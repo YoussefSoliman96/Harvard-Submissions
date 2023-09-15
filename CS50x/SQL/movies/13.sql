ce23d7d2e3db925eb8ba49c396ec5877eda2d860
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-15 00:36:37 +0000

    Fri, Sep 15, 2023, 2:36 AM +02:00

--- CS50x/SQL/movies/13.sql
+++ CS50x/SQL/movies/13.sql
@@ -1,5 +1,6 @@
 write a SQL query to list the names of all people who starred in a movie in which Kevin Bacon also starred.
 
 SELECT name FROM people
-JOIN stars ON stars.person_id = people.id
-JOIN 
+JOIN stars ON stars.movie_id = movies.id
+JOIN movies ON movies.id = stars.movie_id
+WHERE name = "
