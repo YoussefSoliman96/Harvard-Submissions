01e2c6e8b9ba43072f812af414c6f319ff451e1c
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-15 02:02:57 +0000

    Fri, Sep 15, 2023, 4:02 AM +02:00

--- CS50x/SQL/fiftyville/log.sql
+++ CS50x/SQL/fiftyville/log.sql
@@ -5,4 +5,5 @@ SELECT transcript FROM interviews WHERE year = 2021 AND month = 7 AND day = 28;
 Theft of the CS50 duck took place at 10:15am at the Humphrey Street bakery. Interviews were conducted today with three witnesses who were present at the time –
 each of their interview transcripts mentions the bakery. |
 Littering took place at 16:36. No known witnesses.
-SELECT 
+
+SELECT name FROM people WHERE license_plate = (SELECT license_plate FROM bakery_security_logs WHERE year = 2021 AND month = 7 AND day = 28 AND hour = 10); --> Brandon
