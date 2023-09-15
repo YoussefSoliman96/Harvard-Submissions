b3863486f651f86f7ab51946f157e0e13edc4bc7
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-15 02:16:02 +0000

    Fri, Sep 15, 2023, 4:16 AM +02:00

--- CS50x/SQL/fiftyville/log.sql
+++ CS50x/SQL/fiftyville/log.sql
@@ -8,6 +8,5 @@ Littering took place at 16:36. No known witnesses.
 
 SELECT name FROM people WHERE license_plate = (SELECT license_plate FROM bakery_security_logs WHERE year = 2021 AND month = 7 AND day = 28 AND hour = 10); --> Brandon
 
-SELECT id FROM people WHERE people.name = "Brandon"
-JOIN bank_accounts ON bank_accounts.person_id = people.id
-JOIN atm_transactions ON atm_transactions.account_number = bank_account.account_number;
+SELECT account_number FROM bank_accounts
+JOIN account
