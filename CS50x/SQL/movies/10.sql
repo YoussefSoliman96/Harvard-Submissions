876d8aea510596b6b3b27c9637fbc4aa620e8cd0
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-15 00:13:26 +0000

    Fri, Sep 15, 2023, 2:13 AM +02:00

--- CS50x/SQL/movies/10.sql
+++ CS50x/SQL/movies/10.sql
@@ -1,1 +1,5 @@
 write a SQL query to list the names of all people who have directed a movie that received a rating of at least 9.0
+
+SELECT name FROM people
+JOIN directors ON directors.person_id = people.id
+JOIN movies ON movies.id = directors.movie_id
