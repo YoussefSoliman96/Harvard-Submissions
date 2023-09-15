b39a085720bac6f371a260f90ac2c1a94c1ad0d3
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-15 00:51:51 +0000

    Fri, Sep 15, 2023, 2:51 AM +02:00

--- CS50x/SQL/movies/7.sql
+++ CS50x/SQL/movies/7.sql
@@ -1,1 +1,4 @@
-SELECT title, rating FROM (movies JOIN ratings ON movies.id = ratings.movie_id) WHERE year = 2012 ORDER BY rating DESC, title ASC;
+SELECT title, rating FROM movies
+JOIN ratings ON movies.id = ratings.movie_id
+WHERE year = 2012
+ORDER BY rating DESC, title ASC;
