3c4811d359173e8aea535def448a04b94ca5da4f
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-15 04:28:21 +0000

    Fri, Sep 15, 2023, 6:28 AM +02:00

--- CS50x/SQL/fiftyville/log.sql
+++ CS50x/SQL/fiftyville/log.sql
@@ -42,4 +42,4 @@ SELECT passport_number FROM people WHERE name = "Bruce"
 SELECT flight_id FROM passengers WHERE passport_number = (SELECT passport_number FROM people WHERE name = "Diana") --> 36
 SELECT * FROM flights WHERE id = (SELECT flight_id FROM passengers WHERE passport_number = (SELECT passport_number FROM people WHERE name = "Diana"));
 
-SELECT * FROM flights WHERE origin_airport_id = (SELECT id FROM airports WHERE city = "Fiftyville") AND day = 
+SELECT * FROM flights WHERE origin_airport_id = (SELECT id FROM airports WHERE city = "Fiftyville") AND day = 29
