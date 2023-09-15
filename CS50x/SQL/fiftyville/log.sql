4f00a67e465da83630efa3ae1cccb0e3da542f5a
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-15 01:26:54 +0000

    Fri, Sep 15, 2023, 3:26 AM +02:00

--- CS50x/SQL/fiftyville/log.sql
+++ CS50x/SQL/fiftyville/log.sql
@@ -1,2 +1,2 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
-
+SELECT description FROM crime_scene_reports WHERE year = 2021 AND month = "July" AND day = 28 AND street = "Humphrey Street";
