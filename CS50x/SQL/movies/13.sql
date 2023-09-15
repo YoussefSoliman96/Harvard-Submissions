800c9ac4596270ecca0a6f7bc5b693fb5908e950
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-15 01:10:03 +0000

    Fri, Sep 15, 2023, 3:10 AM +02:00

--- CS50x/SQL/movies/13.sql
+++ CS50x/SQL/movies/13.sql
@@ -5,4 +5,4 @@ WHERE movies.id in
 (SELECT movies.id FROM movies
 JOIN stars ON stars.movie_id = movies.id
 JOIN people ON people.id = stars.person_id
-WHERE people.name = "Kevin Bacon")
+WHERE people.name = "Kevin Bacon" AND people.birth = 1958)
