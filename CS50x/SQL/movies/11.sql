3a4e44b7c6fefcfdbcb54665f765eec5ffce9731
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-15 00:30:27 +0000

    Fri, Sep 15, 2023, 2:30 AM +02:00

--- CS50x/SQL/movies/11.sql
+++ CS50x/SQL/movies/11.sql
@@ -1,8 +1,6 @@
-write a SQL query to list the titles of the five highest rated movies (in order) that Chadwick Boseman starred in, starting with the highest rated.
-
-SELECT tile FROM movies
+SELECT title FROM movies
 JOIN ratings ON ratings.movie_id = movies.id
 JOIN stars ON stars.movie_id = movies.id
 JOIN people ON stars.person_id = people.id
 WHERE people.name = "Chadwick Boseman"
-AND
+ORDER BY ratings.rating DESC
---
+++ CS50x/SQL/movies/12.sql
@@ -0,0 +1,6 @@
+write a SQL query to list the titles of all movies in which both Bradley Cooper and Jennifer Lawrence starred
+
+SELECT title FROM movies
+JOIN stars ON stars.movie_id = movies.id
+JOIN people ON people.id = stars.person_id
+WHERE name 
