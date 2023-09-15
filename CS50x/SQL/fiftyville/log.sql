5051d20ee8b19d043fed610e145956b1c67ea967
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-15 02:30:33 +0000

    Fri, Sep 15, 2023, 4:30 AM +02:00

--- CS50x/SQL/fiftyville/log.sql
+++ CS50x/SQL/fiftyville/log.sql
@@ -1,5 +1,5 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
-SELECT description FROM crime_scene_reports WHERE year = 2021 AND month = 7 AND day = 28 AND street = "Blumberg Boulevard";
+SELECT description FROM crime_scene_reports WHERE year = 2021 AND month = 7 AND day = 28 AND street = "Humphrey Street";
 SELECT transcript FROM interviews WHERE year = 2021 AND month = 7 AND day = 28;
 
 Theft of the CS50 duck took place at 10:15am at the Humphrey Street bakery. Interviews were conducted today with three witnesses who were present at the time
