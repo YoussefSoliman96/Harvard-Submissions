f8960278b71207444e1cb617cf4260a5548e5b5d
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-15 04:09:01 +0000

    Fri, Sep 15, 2023, 6:09 AM +02:00

--- CS50x/SQL/fiftyville/log.sql
+++ CS50x/SQL/fiftyville/log.sql
@@ -32,3 +32,8 @@ SELECT * FROM atm_transactions WHERE account_number = 76054385;
 
 26013199
 49610011
+
+SELECT person_id FROM bank_accounts WHERE account_number in (26013199, 49610011)
+SELECT name FROM people WHERE id in (SELECT person_id FROM bank_accounts WHERE account_number in (26013199, 49610011)) --> Diana, Bruce
+SELECT phone_number FROM people WHERE name in ("Diana", "Bruce")
+SELECT day FROM phone_calls WHERE caller in (SELECT phone_number FROM people WHERE name = "Bruce")
