5f43cba230fbd0c68ea936d235759a52eed0378d
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-15 00:22:47 +0000

    Fri, Sep 15, 2023, 2:22 AM +02:00

---
+++ CS50x/SQL/movies/11.sql
@@ -0,0 +1,6 @@
+write a SQL query to list the titles of the five highest rated movies (in order) that Chadwick Boseman starred in, starting with the highest rated.
+
+SELECT tile FROM movies
+JOIN stars ON stars.movie_id = movies.id
+JOIN people ON people.id = stars.person_id
+WHERE name = 
