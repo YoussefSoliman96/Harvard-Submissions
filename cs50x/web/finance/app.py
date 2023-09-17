44b767dfb9484ae45a1027a8720fd9af2d494537
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-17 20:46:59 +0000

    Sun, Sep 17, 2023, 10:46 PM +02:00

--- cs50x/web/finance/app.py
+++ cs50x/web/finance/app.py
@@ -88,7 +88,9 @@ def buy():
 @login_required
 def history():
     """Show history of transactions"""
-    return apology("TODO")
+    user_id = session["user_id"]
+    transactions_db = db.execute("SELECT * FROM transactions WHERE user_id = ?", user_id)
+    return render_template("history.html", transactions = transactions_db)
 
 
 @app.route("/login", methods=["GET", "POST"])
