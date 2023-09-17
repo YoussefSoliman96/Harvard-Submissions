51e9abab2a485563ea5af4bb242996ad66efcde6
YoussefSoliman96 <50502029+YoussefSoliman96@users.noreply.github.com>
2023-09-17 09:52:52 +0000

    Sun, Sep 17, 2023, 11:52 AM +02:00

--- CS50x/web/birthdays/app.py
+++ CS50x/web/birthdays/app.py
@@ -27,7 +27,10 @@ def index():
     if request.method == "POST":
 
         # TODO: Add the user's entry into the database
-        birthdays = db.execute()
+        name = request.form.get.name
+        month = request.form.get.month
+        day = request.form.get.day
+        birthdays = db.execute("INSERT INTO birthdays (name, month, day) VALUES(?, ?, ?)", name, month, day)
 
         return redirect("/")
 
