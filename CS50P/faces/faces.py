387f06fef1959bf274aff7bc1f8c959f1bde1dba
YoussefSoliman96 <youssef.soliman6636@gmail.com>
2023-12-08 17:43:48 +0200

    Fix

--- CS50P/faces/faces.py
+++ CS50P/faces/faces.py
@@ -1,2 +1,13 @@
-def convert (n)
-....if (n = :)) 
+def main():
+    x = input()
+    y = convert(x)
+    print(y)
+
+
+
+def convert (x):
+    x1 = x.replace(':)', '🙂')
+    x2 = x1.replace(':(', '🙁')
+    return x2
+
+
