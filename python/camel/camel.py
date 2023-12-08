65b061b6be863a4e9332b6f8ec5401960a6d6669
YoussefSoliman96 <youssef.soliman6636@gmail.com>
2023-12-08 18:01:32 +0200

    Fix camel

--- python/camel/camel.py
+++ python/camel/camel.py
@@ -6,8 +6,5 @@ for c in x:
         print(y, end="")
     else:
         print(c, end="")
-print()x = input("camelCase: ")
-for c in x:
-    print(c)
-    if c.isupper() = True
-        
+print()
+x = input("camelCase: ")
